9e5658a04b0b2f0e6f9943dfaff7052a6a229db1
Emiel Por <por@strw.leidenuniv.nl>
2016-12-28 16:05:57 +0100

    Set up folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hcipy',
+	version='0.1',
+	description='A framework for performing optical propagation simulations, meant for high contrast imaging, in Python.',
+	url='https://gitlab.strw.leidenuniv.nl/por/hcipy',
+	author='Emiel Por',
+	author_email='por@strw.leidenuniv.nl',
+	packages=['hcipy'],
+	zip_safe=False)

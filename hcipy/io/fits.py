9e92944b0018b11942dff147e9003774d4d7722b
Emiel Por <por@strw.leidenuniv.nl>
2017-02-01 21:35:10 +0100

    Fits reading and writing fields

---
+++ hcipy/io/fits.py
@@ -0,0 +1,20 @@
+def read_fits(filename):
+	from astropy.io import fits
+	return fits.getdata(filename)
+	
+def write_fits(data, filename, shape=None):
+	from astropy.io import fits
+	
+	hdu = None
+	
+	if not shape is None:
+		hdu = fits.PrimaryHDU(data.reshape(shape))
+	elif hasattr(data, 'grid'):
+		if data.grid.is_separated:
+			hdu = fits.PrimaryHDU(data.shaped)
+	
+	if hdu is None:
+		hdu = fits.PrimaryHDU(data)
+
+	hdu.writeto(filename, overwrite=True)
+

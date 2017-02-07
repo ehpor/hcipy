3ee4dc1b1abcf0a1f478537e2e79081a40d97531
Emiel Por <por@strw.leidenuniv.nl>
2017-02-07 01:31:37 +0100

    Adding generic apertures

--- hcipy/aperture/__init__.py
+++ hcipy/aperture/__init__.py
@@ -1,3 +1,5 @@
 __all__ = ['circular_aperture', 'rectangular_aperture', 'regular_polygon_aperture', 'hexagonal_aperture']
+__all__ += []
 
-from .simple import *
+from .generic import *
+from .realistic import *
---
+++ hcipy/aperture/realistic.py
@@ -0,0 +1,1 @@
+# VLT, Subaru, GMT, EELT

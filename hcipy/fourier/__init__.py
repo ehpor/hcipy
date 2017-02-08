c959636b8dd94eeb0732aed46c8846dac7c0a8ad
Emiel Por <por@strw.leidenuniv.nl>
2017-02-08 02:13:04 +0100

    Add all fourier transforms to __all__

---
+++ hcipy/fourier/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['make_fft_grid', 'FastFourierTransform', 'ConvolveFFT', 'RotateFFT', 'FilterFFT']
+__all__ += ['MatrixFourierTransform']
+
+from .fast_fourier_transform import *
+from .matrix_fourier_transform import *

0cc066c1d8f5c19f50245a444111969380d4ecc5
Emiel Por <por@strw.leidenuniv.nl>
2017-02-13 16:47:12 +0100

    Make SVD into a class for easier extension

--- hcipy/math_util/matrix_inversion.py
+++ hcipy/math_util/matrix_inversion.py
@@ -2,29 +2,28 @@ import numpy as np
 
 def inverse_truncated_modal(M, num_modes, svd=None):
 	if svd is None:
-		from .svd import svd as SVD
-		 U, S, Vt = SVD(M, num_modes)
-	else:
-		 U = svd[0][:,:num_modes]
-		 S = svd[1][:num_modes]
-		 Vt = svd[2][:num_modes,:]
+		from .svd import SVD
+		 svd = SVD(M, num_modes)
 
+	U, S, Vt = svd.svd
 	return (Vt.T / S).dot(U.T)
 
 def inverse_truncated(M, rcond=1e-15, svd=None):
 	if svd is None:
-		from .svd import svd as SVD
+		from .svd import SVD
 		svd = SVD(M)
 
+	U, S, Vt = svd.svd
 	S_inv = np.array([1/s if abs(s) > (rocond * S.max()) else 0 for s in S])
 
 	return (Vt.T * S_inv).dot(U.T)
 
 def inverse_tikhonov(M, rcond=1e-15, svd=None):
 	if svd is None:
-		from .svd import svd as SVD
+		from .svd import SVD
 		svd = SVD(M)
 	
+	U, S, Vt = svd.svd
 	S_inv = S / (S**2 + rcond * S.max())**2)
 
 	return (Vt.T * S_inv).dot(U.T)
--- hcipy/math_util/singular_value_decomposition.py
+++ hcipy/math_util/singular_value_decomposition.py
@@ -1,11 +1,59 @@
 import numpy as np
 
-def svd(M, num_modes=None):
-    if num_modes is None:
-        from numpy.linalg import svd
-        return svd(M, full_matrices=False)
-    else:
-        from scipy.sparse.linalg import svds
-        return svds(M, int(num_modes))
+class SVD(object):
+	def __init__(self, M, num_modes=None):
+		self._M = M
+		self._num_modes = num_modes
+		self._svd = None
+
+		if self.num_modes is None:
+			from numpy.linalg import svd
+			self._svd = svd(M, full_matrices=False)
+		else:
+			from scipy.sparse.linalg import svds
+			self._svd = svds(M, int(self.num_modes))
+	
+	@property
+	def left_singular_modes(self):
+		from ..mode_basis import ModeBasis
+		
+		return ModeBasis((m for m in self.U.conj().T))
+	
+	@property
+	def right_singular_modes(self):
+		from ..mode_basis import ModeBasis
+
+		return ModeBasis((m for m in self.Vt.conj()))
+	
+	@property
+	def singular_values(self):
+		return self.S
+	
+	@property
+	def U(self):
+		return self.svd[0]
+	
+	@property
+	def S(self):
+		return self.svd[1]
+	
+	@property
+	def Vt(self):
+		return self.svd[2]
+	
+	def __getitem__(self, i):
+		return self.svd[i]
+	
+	@property
+	def svd(self):
+		return self._svd
+	
+	@property
+	def num_modes(self):
+		return self._num_modes
+	
+	@property
+	def M(self):
+		return self._M
 
 # Incremental SVD planned

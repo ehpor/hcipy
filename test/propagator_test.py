58a34b41feccd920f5b394f1d4662fae64bf7576
Emiel Por <por@strw.leidenuniv.nl>
2017-03-28 12:29:57 +0200

    Simple example for propagations

--- test/propagator_test.py
+++ test/propagator_test.py
@@ -2,23 +2,27 @@ from hcipy import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-pupil_grid = make_pupil_grid(2048, 0.2)
-aperture = circular_aperture(0.1)
+pupil_grid = make_pupil_grid(512)
+aperture = circular_aperture(1)
 
 A = aperture(pupil_grid)
 
 imshow_field(A)
 plt.show()
 
-propagator = FresnelPropagator(pupil_grid, 5)
+focal_grid = make_focal_grid(pupil_grid, 1, 0.1)
 
-wf = Wavefront(A, 500e-9)
-img = propagator(wf)
+fresnel = FresnelPropagator(pupil_grid, 500)
+fraunhofer = FraunhoferPropagator(pupil_grid, focal_grid)
 
-prop_mono = propagator.get_monochromatic_propagator(500e-9)
+wf = Wavefront(A)
+wf.total_power = 1
 
-imshow_field(np.angle(prop_mono.transfer_function), prop_mono.fft.output_grid)
-plt.show()
+img = fresnel(wf)
+print(wf.total_power, img.total_power)
 
-imshow_field(img.intensity / img.intensity.max())
+img = fraunhofer(wf)
+print(wf.total_power, img.total_power)
+
+imshow_field(np.log10(img.intensity / img.intensity.max()), vmin=-5)
 plt.show()

4f3c6e685f59e4b9f84d975261abb49fc8e89041
Emiel Por <por@strw.leidenuniv.nl>
2017-03-29 06:05:38 +0200

    Fix bug

--- hcipy/optics/micro_lens_array.py
+++ hcipy/optics/micro_lens_array.py
@@ -9,9 +9,9 @@ class MicroLensArray(OpticalElement):
 		self.mla_index = Field(-np.ones(self.input_grid.size), self.input_grid)
 		self.mla_opd = Field(np.zeros(self.input_grid.size), self.input_grid)
 
-		for i, (x,y) in self.micro_lens_grid.as_('cartesian').points:
+		for i, (x,y) in enumerate(micro_lens_grid.as_('cartesian').points):
 			shifted_grid = input_grid.shifted((x,y))
-			mask = micro_lens_shape(shifted_grid)
+			mask = micro_lens_shape(shifted_grid) != 0
 
 			self.mla_opd[mask] = (-1 / (2*focal_length)) * (shifted_grid.x[mask]**2 + shifted_grid.y[mask]**2)
 			self.mla_index[mask] = i

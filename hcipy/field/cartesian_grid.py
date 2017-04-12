Fix weight scaling
Scaling would be applied double if the weights are calculated on the fly (based on the just scaled coordinates).

hcipy/field/cartesian_grid.py
@@ -22,8 +22,8 @@ class CartesianGrid(Grid):
 		return self.coords[3]
 	
 	def scale(self, scale):
-		self.coords *= scale
 		self.weights *= np.abs(scale)**self.ndim
+		self.coords *= scale
 
 	def shift(self, shift):
 		self.coords += shift
c43cc9185fae91d38c1a44945ee10da8f4a0a67b
Emiel Por <por@strw.leidenuniv.nl>
2017-04-15 22:35:07 +0200

    Add Fourier mode basis

--- hcipy/mode_basis/__init__.py
+++ hcipy/mode_basis/__init__.py
@@ -2,6 +2,7 @@ __all__ = ['ModeBasis']
 __all__ += ['make_zernike_basis', 'zernike']
 
 from .disk_harmonic import *
+from .fourier import *
 from .karhunen_loeve import *
 from .mode_basis import *
 from .zernike import *

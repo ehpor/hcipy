8a55b532052e82d2c04e969de9f763edfb3d8102
Emiel Por <por@strw.leidenuniv.nl>
2017-04-20 13:34:59 +0200

    Fix merge conflict

--- hcipy/mode_basis/__init__.py
+++ hcipy/mode_basis/__init__.py
@@ -3,6 +3,7 @@ __all__ += ['make_gaussian_pokes']
 __all__ += ['make_zernike_basis', 'zernike']
 
 from .disk_harmonic import *
+from .fourier import *
 from .gaussian_pokes import *
 from .karhunen_loeve import *
 from .mode_basis import *

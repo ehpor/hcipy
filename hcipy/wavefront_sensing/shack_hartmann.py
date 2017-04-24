c0b66cee90436757e07937ba7fd1d2dc003b4bce
Vikram Mark  Radhakrishnan <radhakrishnan@angstel.strw.leidenuniv.nl>
2017-04-24 16:49:22 +0200

    Modified the SHWFS to have only the optics, no detector. Added helper class for square SHWFS. Modified MLA to make focal length accessible

--- hcipy/wavefront_sensing/shack_hartmann.py
+++ hcipy/wavefront_sensing/shack_hartmann.py
@@ -4,39 +4,24 @@ from ..propagation import FresnelPropagator
 
 import numpy as np
 
-def calculate_centroids(intensity_field, mla_grid, mla_index):
-    pass
-
 class ShackHartmannWavefrontSensor(WavefrontSensorOptics):
-    def __init__(self, input_grid, micro_lens_shape, f_number, N_lenslets, wavelength, detector):
+    def __init__(self, input_grid, micro_lens_array):
+        
+        # Make propagator
+        sh_prop = FresnelPropagator(input_grid, micro_lens_array.focal_length)
+        
+        # Make optical system
+        self.optical_system = OpticalSystem((sh_prop, microlens_array, sh_prop))
+        
+class SquareShackHartmannWavefrontSensor(ShackHartmannWavefrontSensor):
+    ## Helper class to create a Shack-Hartmann WFS with square microlens array
+    def __init__(self, input_grid, f_number, N_lenslets):
         
         diameter = 1.0 / N_lenslets
         x = np.arange(-1,1,diameter)
-        self.mla_grid = CartesianGrid(SeparatedCoords((x,x)))
+        mla_grid = CartesianGrid(SeparatedCoords((x,x)))
         
-        self.wavelength = wavelength
+        mla_shape = rectangular_aperture(diameter)
+        mla = MicroLensArray(input_grid, mla_grid, mla_shape, f_number * diameter)
         
-        self.detector = detector()
-        
-        # Make microlens array
-        microlens_array = MicroLensArray(input_grid, self.mla_grid, micro_lens_shape, f_number * diameter)
-        self.mla_index = microlens_array.mla_index        
-        
-        # Make propagator
-        sh_prop = FresnelPropagator(input_grid, f_number * diameter)
-        
-        
-        self.optical_system = OpticalSystem((microlens_array, sh_prop))
-        
-    def integrate(self, wavefront, dt=1, weight=1):
-        wf = self.optical_system.forward(wavefront)
-        self.detector.integrate(wf, dt, weight)
-    
-    def read_out(self):
-        
-        # Calculate centroids
-        self.detector_image = self.detector.read_out()
-        centroids = calculate_centroids(self.detector_image, self.mla_grid, self.mla_index)
-        
-        # return centroids in np.array()
-        return centroids
+        ShackHartmannWavefrontSensor.__init__(input_grid, mla)

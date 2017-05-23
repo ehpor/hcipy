4e38d9c90b31312aa398f64f8231d0996661d83f
Emiel Por <por@strw.leidenuniv.nl>
2017-05-23 14:56:26 +0200

    Add convolution approach to VortexCoronagraph.

--- examples/vortex.py
+++ examples/vortex.py
@@ -2,23 +2,24 @@ from hcipy import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-pupil_grid = make_pupil_grid(256)
+pupil_grid = make_pupil_grid(1024)
 focal_grid = make_focal_grid(pupil_grid, 16, 32)
 prop = FraunhoferPropagator(pupil_grid, focal_grid)
 
-vortex = VortexCoronagraph(pupil_grid, 1, 2, 'GS')
+vortex = VortexCoronagraph(pupil_grid, 1, 2, 'none', 32, 32)
 
 aperture = circular_aperture(1)(pupil_grid)
 wf = Wavefront(aperture)
 wf2 = vortex(wf)
+wf3 = wf2.copy()
 #wf2 = wf
-wf2.electric_field *= aperture
-wf3 = prop(wf2)
+wf3.electric_field *= aperture
+wf4 = prop(wf3)
 
 imshow_field(np.log10(wf2.intensity))
 plt.colorbar()
 plt.show()
 
-imshow_field(np.log10(wf3.intensity))
+imshow_field(np.log10(wf4.intensity))
 plt.colorbar()
 plt.show()
--- hcipy/coronagraphy/vortex.py
+++ hcipy/coronagraphy/vortex.py
@@ -3,6 +3,8 @@ from ..optics import OpticalElement, Wavefront
 from ..propagation import FraunhoferPropagator
 from ..field import make_focal_grid, Field
 from ..aperture import circular_aperture
+from ..fourier import FastFourierTransform
+from scipy.special import jv
 
 import matplotlib.pyplot as plt
 from ..plotting import imshow_field
@@ -13,7 +15,9 @@ class VortexCoronagraph(OpticalElement):
 		self.focal_grid = make_focal_grid(pupil_grid, q, num_airy)
 		self.prop = FraunhoferPropagator(pupil_grid, self.focal_grid)
 
-		if method == 'GS':
+		self.method = method.lower()
+
+		if self.method == 'gs':
 			aperture = circular_aperture(pupil_diameter)(pupil_grid)
 			wf = Wavefront(aperture)
 
@@ -30,15 +34,51 @@ class VortexCoronagraph(OpticalElement):
 				foc2 = self.prop(pup)
 				self.vortex *= 1 - foc2.electric_field / foc.electric_field
 
-			imshow_field(np.angle(self.vortex), self.focal_grid)
+			imshow_field(np.abs(self.vortex), self.focal_grid, vmax=1.2)
 			plt.colorbar()
 			plt.show()
-		elif method is 'none':
+		elif self.method.lower() == 'none':
 			self.vortex = np.exp(1j * charge * self.focal_grid.as_('polar').theta)
+		elif self.method.lower() == 'convolution':
+			pup_pol = pupil_grid.as_('polar')
+			L = 512 # size of the focal-plane mask in lambda/D
+			r_scaled = 2*np.pi * L * pup_pol.r
+
+			if charge == 2:
+				F = np.exp(2j * pup_pol.theta) / (np.pi * pup_pol.r**2) * (-1 + jv(0,r_scaled) + 0.5 * r_scaled * jv(1, r_scaled))
+			elif charge == 4:
+				F = np.exp(4j * pup_pol.theta) / (np.pi * pup_pol.r**2) * (2 + 4 * jv(0, r_scaled) + (0.5 * r_scaled - 12 / r_scaled) * jv(1, r_scaled))
+			elif charge > 4:
+				pass
+			
+			F *= circular_aperture(pupil_diameter)(pupil_grid)
+			self.fourier = FastFourierTransform(pupil_grid, 4, 1)
+			self.F = self.fourier.forward(F)
+
+			plt.subplot(1,2,1)
+			imshow_field(np.abs(F), self.fourier.input_grid)
+			plt.colorbar()
+			plt.subplot(1,2,2)
+			imshow_field(np.angle(F), self.fourier.input_grid)
+			plt.colorbar()
+			plt.figure()
+
+			plt.subplot(1,2,1)
+			imshow_field(np.abs(self.F), self.fourier.output_grid)
+			plt.colorbar()
+			plt.subplot(1,2,2)
+			imshow_field(np.angle(self.F), self.fourier.output_grid)
+			plt.colorbar()
+			plt.show()
 
 	def forward(self, wavefront):
-		foc = self.prop(wavefront)
+		if self.method == 'gs' or self.method == 'none':
+			foc = self.prop(wavefront)
+			foc.electric_field *= self.vortex
+			return self.prop.backward(foc)
+		elif self.method == 'convolution':
+			ft = self.fourier.forward(wavefront.electric_field)
+			ft *= self.F
+			conv = self.fourier.backward(ft)
 
-		foc.electric_field *= self.vortex
-
-		return self.prop.backward(foc)
+			return Wavefront(conv, wavefront.wavelength)

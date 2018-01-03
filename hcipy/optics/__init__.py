02fd5e243082a2884d2e02bd6e9bc40a6e356515
Emiel Por <por@strw.leidenuniv.nl>
2018-01-03 04:02:41 +0100

    Add Gaussian beam.

--- hcipy/optics/__init__.py
+++ hcipy/optics/__init__.py
@@ -3,6 +3,7 @@ __all__ += ['DynamicOpticalSystem']
 __all__ += ['DeformableMirror']
 __all__ += ['Detector', 'PerfectDetector', 'NoisyDetector']
 __all__ += ['SingleModeFiber', 'fiber_mode_gaussian', 'SingleModeFiberArray']
+__all__ += ['GaussianBeam']
 __all__ += ['MicroLensArray', 'closest_points']
 __all__ += ['AtmosphericModel', 'kolmogorov_psd', 'von_karman_psd', 'modified_von_karman_psd', 'make_standard_multilayer_atmosphere', 'scale_Cn_squared_to_fried_parameter', 'get_fried_parameter']
 __all__ += ['OpticalElement', 'OpticalSystem']
@@ -14,6 +15,7 @@ from .dynamic_optical_system import *
 from .deformable_mirror import *
 from .detector import *
 from .fiber import *
+from .gaussian_beam import *
 from .micro_lens_array import *
 from .multi_layer_atmosphere import *
 from .optical_element import *

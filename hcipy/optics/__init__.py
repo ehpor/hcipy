b5dca40b912f080e417a8ca709b646b914dffe42
Emiel Por <por@strw.leidenuniv.nl>
2018-01-03 01:39:58 +0100

    Add support for monochromatic optical elements

--- hcipy/optics/__init__.py
+++ hcipy/optics/__init__.py
@@ -6,6 +6,7 @@ __all__ += ['SingleModeFiber', 'fiber_mode_gaussian', 'SingleModeFiberArray']
 __all__ += ['MicroLensArray', 'closest_points']
 __all__ += ['AtmosphericModel', 'kolmogorov_psd', 'von_karman_psd', 'modified_von_karman_psd', 'make_standard_multilayer_atmosphere', 'scale_Cn_squared_to_fried_parameter', 'get_fried_parameter']
 __all__ += ['OpticalElement', 'OpticalSystem']
+__all__ += ['PhaseRetarder', 'LinearRetarder', 'CircularRetarder', 'QuarterWavePlate', 'HalfWavePlate']
 __all__ += ['Wavefront']
 
 from .apodization import *
@@ -16,4 +17,5 @@ from .fiber import *
 from .micro_lens_array import *
 from .multi_layer_atmosphere import *
 from .optical_element import *
+from .polarization import *
 from .wavefront import *
--- hcipy/optics/optical_element.py
+++ hcipy/optics/optical_element.py
@@ -1,4 +1,6 @@
 import numpy as np
+import inspect
+import copy
 
 class OpticalElement(object):
 	def __call__(self, wavefront):
@@ -15,6 +17,78 @@ class OpticalElement(object):
 	
 	def get_transformation_matrix_backward(self, wavelength=1):
 		raise NotImplementedError()
+	
+	def get_instance(self, input_grid, wavelength):
+		return self
+
+def make_polychromatic(evaluated_arguments=None, num_in_cache=50):
+	def decorator(optical_element):
+		class PolychromaticOpticalElement(OpticalElement):
+			def __init__(self, *args, **kwargs):
+				self.wavelengths = []
+				self.monochromatic_optical_elements = []
+				self.monochromatic_args = args
+				self.monochromatic_kwargs = kwargs
+
+				if evaluated_arguments is not None:
+					init = optical_element.__init__
+					if hasattr(inspect, 'signature'):
+						# Python 3
+						monochromatic_arg_names = list(inspect.signature(init).parameters.keys())[1:]
+					else:
+						# Python 2
+						monochromatic_arg_names = inspect.getfullargspec(init)[1:]
+					
+					self.evaluate_arg = [m in evaluated_arguments for m in monochromatic_arg_names]
+
+			def get_instance(self, input_grid, wavelength):
+				if self.wavelengths:
+					i = np.argmin(np.abs(wavelength - np.array(self.wavelengths)))
+					wavelength_closest = self.wavelengths[i]
+
+					delta_wavelength = np.abs(wavelength - wavelength_closest)
+					if (delta_wavelength / wavelength) < 1e-6:
+						return self.monochromatic_propagators[i]
+				
+				if evaluated_arguments is not None:
+					args = list(self.monochromatic_args)
+					kwargs = dict(self.monochromatic_kwargs)
+
+					for i, (arg, ev) in enumerate(zip(args, self.evaluate_arg)):
+						if ev and callable(arg):
+							args[i] = arg(wavelength)
+					
+					for key, val in kwargs.items():
+						if key in evaluated_arguments and callable(val):
+							kwargs[key] = val(wavelength)
+					
+					elem = optical_element(*args, wavelength=wavelength, **kwargs)
+				else:
+					elem = optical_element(*self.monochromatic_args, wavelength=wavelength, **self.monochromatic_kwargs)
+				
+				self.wavelengths.append(wavelength)
+				self.monochromatic_optical_elements.append(elem)
+
+				if len(self.wavelengths) > num_in_cache:
+					self.wavelengths.pop(0)
+					self.monochromatic_optical_elements.pop(0)
+				
+				return elem
+			
+			def forward(self, wavefront):
+				return self.get_instance(self, wavefront.electric_field.grid, wavefront.wavelength).forward(wavefront)
+			
+			def backward(self, wavefront):
+				return self.get_instance(self, wavefront.electric_field.grid, wavefront.wavelength).backward(wavefront)
+			
+			def get_transformation_matrix_forward(self, input_grid, wavefront):
+				return self.get_instance(self, input_grid, wavelength).get_transformation_matrix_forward(input_grid, wavelength)
+			
+			def get_transformation_matrix_backward(self, input_grid, wavefront):
+				return self.get_instance(self, input_grid, wavelength).get_transformation_matrix_backward(input_grid, wavelength)
+		
+		return PolychromaticOpticalElement
+	return decorator
 
 class OpticalSystem(OpticalElement):
 	def __init__(self, optical_elements):

900e64e2db0878eb054b78125e49e50e34e5fa73
Emiel Por <por@strw.leidenuniv.nl>
2018-03-13 10:58:51 +0100

    Fix spatial axis bug.

--- hcipy/field/operations.py
+++ hcipy/field/operations.py
@@ -1,4 +1,5 @@
 from .field import Field
+import numpy as np
 
 def field_inverse_tikhonov(f, rcond=1e-15):
 	from ..math_util import inverse_tikhonov
@@ -6,7 +7,7 @@ def field_inverse_tikhonov(f, rcond=1e-15):
 	res = []
 	for i in range(f.grid.size):
 		res.append(inverse_tikhonov(f[...,i], rcond))
-	return Field(res, f.grid)
+	return Field(np.moveaxis(res, 0, -1), f.grid)
 
 def make_field_operation(op):
 	pass

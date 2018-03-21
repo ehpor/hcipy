a6237b8a743c298d9f8c68b963994908b81315c0
Emiel Por <por@strw.leidenuniv.nl>
2018-03-21 23:24:35 +0100

    Add documentation and field_svd().

--- hcipy/field/operations.py
+++ hcipy/field/operations.py
@@ -2,12 +2,73 @@ from .field import Field
 import numpy as np
 
 def field_inverse_tikhonov(f, rcond=1e-15):
+	'''Invert a tensor field of order 2 using Tikhonov regularization.
+
+	Parameters
+	----------
+	f : `Field`
+		The tensor field for which to calculate the inverses. The tensor order
+		of this Field has to be 2.
+	rcond : scalar
+		The relative regularization parameter to use for the inversions.
+	
+	Returns
+	-------
+	Field
+		The resulting Field with tensor order 2.
+	
+	Raises
+	------
+	ValueError
+		If the tensor order of field `f` is not 2.
+	'''
 	from ..math_util import inverse_tikhonov
+
+	if f.tensor_order != 2:
+		raise ValueError("Field must be a tensor field of order 2 to be able to calculate inverses.")
 
 	res = []
 	for i in range(f.grid.size):
 		res.append(inverse_tikhonov(f[...,i], rcond))
 	return Field(np.moveaxis(res, 0, -1), f.grid)
+
+def field_svd(f, full_matrices=True, compute_uv=True):
+	'''Calculate the singular value decomposition for a tensor field of order 2.
+
+	Parameters
+	----------
+	f : `Field`
+		The tensor field for which to calculate the singular value decompositions.
+	full_matrices : boolean
+		If True, matrices in U and Vh will have shapes (M,M) and (N,N) respectively.
+		Otherwise their shapes are (M,K), (K,N) respectively, where K=min(M,N).
+	compute_uv : boolean
+		Whether to compute matrices U and Vh in addition to the singular values.
+	
+	Returns
+	U : `Field`
+		The unitary matrices. Only returned if `compute_uv` is True.
+	S : `Field`
+		The singular values, sorted in descending order.
+	Vh : `Field`
+		The unitary matrices. Only returned if `compute_uv` is True.
+	'''
+	
+	res = np.linalg.svd(np.moveaxis(f, -1, 0), full_matrices, compute_uv)
+
+	if compute_uv:
+		U, S, Vh = res
+		U = Field(np.moveaxis(U, 0, -1), f.grid)
+		Vh = Field(np.moveaxis(Vh, 0, -1), f.grid)
+	else:
+		S = res
+	
+	S = Field(np.moveaxis(S, 0, -1), f.grid)
+	
+	if compute_uv:
+		return U, S, Vh
+	else:
+		return S
 
 def make_field_operation(op):
 	pass

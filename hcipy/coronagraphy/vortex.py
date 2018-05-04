b33a66e582ae4b5337bc49610b8c92155362a6ee
Emiel Por <por@strw.leidenuniv.nl>
2018-05-04 20:37:51 +0200

    Increase efficiency of the vortex coronagraph.
    The propagation now uses a multi-scale approach to resolve the vortex singularity.

--- hcipy/coronagraphy/vortex.py
+++ hcipy/coronagraphy/vortex.py
@@ -1,122 +1,76 @@
 import numpy as np
 from ..optics import OpticalElement, Wavefront
 from ..propagation import FraunhoferPropagator
-from ..field import make_focal_grid, Field
-from ..aperture import circular_aperture
-from ..fourier import FastFourierTransform
+from ..field import make_focal_grid, Field, make_pupil_grid
+from ..aperture import circular_aperture, rectangular_aperture
+from ..fourier import FastFourierTransform, MatrixFourierTransform
 from scipy.special import jv
 
 import matplotlib.pyplot as plt
 from ..plotting import imshow_field, complex_field_to_rgb
 
 class VortexCoronagraph(OpticalElement):
-	def __init__(self, pupil_grid, pupil_diameter=1, charge=2, method='gs', q=8, num_airy=128):
-		self.pupil_grid = pupil_grid
-		self.focal_grid = make_focal_grid(pupil_grid, q, num_airy)
-		self.prop = FraunhoferPropagator(pupil_grid, self.focal_grid)
+	def __init__(self, input_grid, pupil_diameter=1, charge=2, scalings=4, scaling_factor=4):
+		self.input_grid = input_grid
+		
+		q_outer = 2
+		num_airy_outer = input_grid.shape[0]/2
 
-		self.method = method.lower()
+		focal_grids = []
+		self.focal_masks = []
+		self.props = []
 
-		if self.method == 'gs':
-			aperture = circular_aperture(pupil_diameter)(pupil_grid)
-			wf = Wavefront(aperture)
+		for i in range(scalings):
+			q = q_outer * scaling_factor**i
+			if i == 0:
+				num_airy = num_airy_outer
+			else:
+				num_airy = 32 / (q_outer * scaling_factor**(i-1))
 
-			self.vortex = np.exp(1j * charge * self.focal_grid.as_('polar').theta)
-			self.vortex = Field(self.vortex, self.focal_grid)
-			i = self.focal_grid.closest_to((0,0))
-			self.vortex[i] = 0
+			focal_grid = make_focal_grid(input_grid, q, num_airy)
+			print(focal_grid.shape)
+			focal_mask = Field(np.exp(1j * charge * focal_grid.as_('polar').theta), focal_grid)
 
-			foc = self.prop(wf)
+			focal_mask *= 1 - circular_aperture(1e-9)(focal_grid)
 
-			for i in range(100):
-				foc2 = foc.copy()
-				foc2.electric_field *= self.vortex
-				foc2.electric_field *= circular_aperture(num_airy*2)(self.focal_grid)
+			for j in range(i):
+				fft = FastFourierTransform(focal_grids[j])
+				mft = MatrixFourierTransform(focal_grid, fft.output_grid)
 
-				pup = self.prop.backward(foc2)
-				pup.electric_field[aperture < 0.5] = 0
-
-				foc3 = self.prop(pup)
-				#foc3.electric_field /= self.vortex
-
-				self.vortex -= foc3.electric_field / foc.electric_field
-				self.vortex = np.exp(1j * np.angle(self.vortex))
-
-			#plt.subplot(1,2,1)
-			#imshow_field(np.abs(self.vortex), self.focal_grid, vmax=1.2)
-			#plt.colorbar()
-			#plt.subplot(1,2,2)
-			#imshow_field(np.angle(self.vortex), self.focal_grid)
-			#plt.colorbar()
-			#plt.show()
-		elif self.method.lower() == 'none':
-			self.vortex = np.exp(1j * charge * self.focal_grid.as_('polar').theta)
-			self.vortex = Field(self.vortex, self.focal_grid)
-			i = self.focal_grid.closest_to((0,0))
-			self.vortex[i] = 0
-		elif self.method.lower() == 'convolution':
-			pup_pol = pupil_grid.as_('polar')
-			L = 512 # size of the focal-plane mask in lambda/D
-			r_scaled = 2*np.pi * L * pup_pol.r
-
-			if charge == 2:
-				F = np.exp(2j * pup_pol.theta) / (np.pi * pup_pol.r**2) * (-1 + jv(0,r_scaled) + 0.5 * r_scaled * jv(1, r_scaled))
-			elif charge == 4:
-				F = np.exp(4j * pup_pol.theta) / (np.pi * pup_pol.r**2) * (2 + 4 * jv(0, r_scaled) + (0.5 * r_scaled - 12 / r_scaled) * jv(1, r_scaled))
-			elif charge > 4:
-				pass
+				focal_mask -= mft.backward(fft.forward(self.focal_masks[j]))
 			
-			F *= circular_aperture(pupil_diameter)(pupil_grid)
-			self.fourier = FastFourierTransform(pupil_grid, 4, 1)
-			self.F = self.fourier.forward(F)
+			prop = FraunhoferPropagator(input_grid, focal_grid)
 
-			plt.subplot(1,2,1)
-			imshow_field(np.abs(F), self.fourier.input_grid)
-			plt.colorbar()
-			plt.subplot(1,2,2)
-			imshow_field(np.angle(F), self.fourier.input_grid)
-			plt.colorbar()
-			plt.figure()
-
-			plt.subplot(1,2,1)
-			imshow_field(np.abs(self.F), self.fourier.output_grid)
-			plt.colorbar()
-			plt.subplot(1,2,2)
-			imshow_field(np.angle(self.F), self.fourier.output_grid)
-			plt.colorbar()
-			plt.show()
-		elif method.lower() == 'inversion':
-			aperture = circular_aperture(pupil_diameter)(pupil_grid)
-			wf = Wavefront(aperture)
-			wf.total_power = 1
-
-			self.vortex = np.exp(1j * charge * self.focal_grid.as_('polar').theta)
-			self.vortex = Field(self.vortex, self.focal_grid)
-			self.vortex *= circular_aperture(num_airy*2)(self.focal_grid)
-
-			i = self.focal_grid.closest_to((0,0))
-			self.vortex[i] = 0
-
-			foc = self.prop.forward(wf)
-			foc_copy = foc.copy()
-			foc.electric_field *= self.vortex
-
-			pup = self.prop.backward(foc)
-			pup.electric_field[aperture < 0.5] = 0
-			
-			self.reference = pup.electric_field
+			focal_grids.append(focal_grid)
+			self.focal_masks.append(focal_mask)
+			self.props.append(prop)
 
 	def forward(self, wavefront):
-		if self.method == 'gs' or self.method == 'none' or self.method == 'inversion':
-			foc = self.prop(wavefront)
-			foc.electric_field *= self.vortex
-			res = self.prop.backward(foc)
-			if self.method == 'inversion':
-				res.electric_field -= self.reference * np.sqrt(wavefront.total_power)
-			return res
-		elif self.method == 'convolution':
-			ft = self.fourier.forward(wavefront.electric_field)
-			ft *= self.F
-			conv = self.fourier.backward(ft)
+		wavelength = wavefront.wavelength
+		wavefront.wavelength = 1
 
-			return Wavefront(conv, wavefront.wavelength)
+		for i, (mask, prop) in enumerate(zip(self.focal_masks, self.props)):
+			focal = prop(wavefront)
+			focal.electric_field *= mask
+			if i == 0:
+				lyot = prop.backward(focal)
+			else:
+				lyot.electric_field += prop.backward(focal).electric_field
+
+		lyot.wavelength = wavelength
+		return lyot
+	
+	def backward(self, wavefront):
+		wavelength = wavefront.wavelength
+		wavefront.wavelength = 1
+
+		for i, (mask, prop) in enumerate(zip(self.focal_masks, self.props)):
+			focal = prop(wavefront)
+			focal.electric_field *= mask.conj()
+			if i == 0:
+				pup = prop.backward(focal)
+			else:
+				pup.electric_field += prop.backward(focal).electric_field
+
+		pup.wavelength = wavelength
+		return pup

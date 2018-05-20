a5a165035df0a0bd40b1bebb75de228ede348207
Emiel Por <por@strw.leidenuniv.nl>
2018-05-20 21:53:26 +0200

    Fix imports and internal variable names.

--- hcipy/optics/aberration.py
+++ hcipy/optics/aberration.py
@@ -1,6 +1,13 @@
+import numpy as np
+
 from ..field import Field
 from ..statistics import SpectralNoiseFactoryFFT
 from .apodization import SurfaceApodizer
+from ..propagation import FresnelPropagator
+from ..mode_basis import make_zernike_basis
+from ..math_util import inverse_tikhonov
+from ..aperture import circular_aperture
+from .optical_element import OpticalElement
 
 def make_power_law_error(pupil_grid, ptv, diameter, exponent=-2.5, aperture=None, remove_modes=None):
 	def psd(grid):
@@ -22,22 +29,22 @@ def make_power_law_error(pupil_grid, ptv, diameter, exponent=-2.5, aperture=None
 	
 	return Field(np.exp(2j*np.pi * screen) * aperture, pupil_grid)
 
-class SurfaceError(SurfaceApodizer):
+class SurfaceAberration(SurfaceApodizer):
 	def __init__(self, pupil_grid, ptv, diameter, exponent=-2.5, refractive_index=-1, aperture=None, remove_modes=None):
 		surface = make_power_law_error(pupil_grid, ptv, diameter, exponent, aperture, remove_modes)
 		SurfaceApodizer.__init__(self, surface, refractive_index)
 
-class SurfaceAtDistance(OpticalElement):
-	def __init__(self, surface_error, distance):
-		self.fresnel = FresnelPropagator(pupil_grid, distance)
-		self.surface = surface_error
+class SurfaceAberrationAtDistance(OpticalElement):
+	def __init__(self, surface_aberration, distance):
+		self.fresnel = FresnelPropagator(surface_aberration.input_grid, distance)
+		self.surface_aberration = surface_aberration
 
 	def forward(self, wavefront):
 		wf = self.fresnel.forward(wavefront)
-		wf = self.surface.forward(wf)
+		wf = self.surface_aberration.forward(wf)
 		return self.fresnel.backward(wf)
 	
 	def backward(self, wavefront):
 		wf = self.fresnel.forward(wavefront)
-		wf = self.surface.backward(wf)
+		wf = self.surface_aberration.backward(wf)
 		return self.fresnel.backward(wf)

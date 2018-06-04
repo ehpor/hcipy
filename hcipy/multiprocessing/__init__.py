0218d56b286c50508bcc650ca8d3c8e2da978283
Emiel Por <por@strw.leidenuniv.nl>
2018-06-04 21:45:11 +0200

    Remove multiprocessing and simplify shack hartmann.

--- hcipy/multiprocessing/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['mpi_map']
-__all__ += ['par_map']
-__all__ += ['par_map_reduce', 'reduce_add', 'reduce_multiply']
-
-from .mpi_map import *
-from .par_map import par_map
-from .par_map_reduce import par_map_reduce, reduce_add, reduce_multiply

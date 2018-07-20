f0bac0b4a3602ac03fac105d839995eefaa829f5
Emiel Por <por@strw.leidenuniv.nl>
2018-07-20 16:01:28 +0200

    Add documentation.

--- hcipy/optics/dynamic_optical_system.py
+++ hcipy/optics/dynamic_optical_system.py
@@ -2,12 +2,28 @@ import heapq
 import numpy as np
 
 class DynamicOpticalSystem(object):
+	'''A dynamically varying optical system.
+
+	This can be used as a starting point for an implementation of an adaptive optics system. The optics 
+	are changed as specific moments in time, which can be scheduled by submitting a callback. The time
+	points are bridged using the integrate function, which is written by the derived class. This 
+	function should integrate on each detector without reading out the detector. Detector readouts or 
+	DM changes should be implemented as callbacks.
+	'''
 	def __init__(self):
 		self.callbacks = []
 		self.t = 0
 		self.callback_counter = 0
 	
 	def evolve_until(self, t):
+		'''Evolve the optical system until time `t`.
+
+		Parameters
+		----------
+		t : scalar
+			The point in time to which to simulate this optical system. Callbacks and integrations
+			are handled along the way.
+		'''
 		if t < self.t:
 			raise ValueError('Backwards evolution is not allowed.')
 		
@@ -31,10 +47,32 @@ class DynamicOpticalSystem(object):
 				callback()
 	
 	def integrate(self, integration_time):
+		'''Integrate the current optical system for a certain integration time.
+
+		This function should be implemented by a user in a derived class.
+
+		Parameters
+		----------
+		integration_time : scalar
+			The integration time with which to integrate the optical system.
+		'''
 		pass
 	
 	def add_callback(self, t, callback):
+		'''Add a callback to the list of callbacks.
+
+		This function can even be called during handling of a callback. This is 
+		especially useful when implementing periodic callbacks: a callback can
+		reinsert itself at a later time at the end of handling of the callback.
+
+		Parameters
+		----------
+		t : scalar
+			The time at which to call the callback.
+		callback : function
+			The function to call at time `t`. This function should have no arguments.
+		'''
 		# Callback counter is to avoid comparison of callback functions
-		# (which you can't), if the times are equal.
+		# if the times are equal, as comparison of functions is not allowed.
 		heapq.heappush(self.callbacks, (t, self.callback_counter, callback))
 		self.callback_counter += 1

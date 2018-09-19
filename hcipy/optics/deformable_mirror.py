281ef7b69f23dc740977d033a4d7ebdcce142e60
Emiel Por <por@strw.leidenuniv.nl>
2018-09-19 10:10:28 +0200

    Add phase_for() function and add documentation.

--- hcipy/optics/deformable_mirror.py
+++ hcipy/optics/deformable_mirror.py
@@ -4,23 +4,59 @@ from ..field import Field
 from ..mode_basis import ModeBasis
 
 class DeformableMirror(OpticalElement):
+	'''A deformable mirror using influence functions.
+
+	This class does not contain any temporal simulation (ie. settling time),
+	and assumes that there is no crosstalk between actuators.
+
+	Parameters
+	----------
+	influence_functions : ModeBasis
+		The influence function for each of the actuators.
+	'''
 	def __init__(self, influence_functions):
 		self.influence_functions = influence_functions
 		self.actuators = np.zeros(len(influence_functions))
 		self.input_grid = influence_functions[0].grid
 	
 	def forward(self, wavefront):
+		'''Propagate a wavefront through the deformable mirror.
+
+		Parameters
+		----------
+		wavefront : Wavefront
+			The incoming wavefront.
+		
+		Returns
+		-------
+		Wavefront
+			The reflected wavefront.
+		'''
 		wf = wavefront.copy()
 		wf.electric_field *= np.exp(2j * self.surface * wavefront.wavenumber)
 		return wf
 	
 	def backward(self, wavefront):
+		'''Propagate a wavefront backwards through the deformable mirror.
+
+		Parameters
+		----------
+		wavefront : Wavefront
+			The incoming wavefront.
+		
+		Returns
+		-------
+		Wavefront
+			The reflected wavefront.
+		'''
 		wf = wavefront.copy()
 		wf.electric_field *= np.exp(-2j * self.surface * wavefront.wavenumber)
 		return wf
 	
 	@property
 	def influence_functions(self):
+		'''The influence function for each of the actuators of this deformable mirror.
+		'''
 		return self._influence_functions
 	
 	@influence_functions.setter
@@ -30,5 +66,22 @@ class DeformableMirror(OpticalElement):
 	
 	@property
 	def surface(self):
+		'''The surface of the deformable mirror in meters.
+		'''
 		surf = self._transformation_matrix.dot(self.actuators)
 		return Field(surf, self.input_grid)
+	
+	def phase_for(self, wavelength):
+		'''Get the phase that is added to a wavefront with a specified wavelength.
+
+		Parameters
+		----------
+		wavelength : scalar
+			The wavelength at which to calculate the phase deformation.
+		
+		Returns
+		-------
+		Field
+			The calculated phase deformation.
+		'''
+		return 2 * self.surface * wavefront.wavenumber

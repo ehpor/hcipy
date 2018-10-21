cc8243f5612ee32c7555ace9bb00a529ab0b3ced
Emiel Por <por@strw.leidenuniv.nl>
2018-10-21 23:30:55 +0200

    Add Xinetics DM model, including DM tilt.

--- hcipy/optics/deformable_mirror.py
+++ hcipy/optics/deformable_mirror.py
@@ -1,7 +1,49 @@
 import numpy as np
+import pkg_resources
+
 from .optical_element import OpticalElement
-from ..field import Field
+from ..field import Field, make_uniform_grid
 from ..mode_basis import ModeBasis
+from ..interpolation import make_linear_interpolator_separated
+from ..io import read_fits
+
+def make_xinetics_influence_functions(pupil_grid, num_actuators_across_pupil, actuator_spacing, x_tilt=0, y_tilt=0, z_tilt=0):
+	'''Create influence functions for a Xinetics deformable mirror.
+
+	This function uses a The rotation of the deformable mirror will be done in the order X-Y-Z.
+
+	Parameters
+	----------
+	pupil_grid : Grid
+		The grid on which to calculate the influence functions.
+	num_actuators_across_pupil : integer
+		The number of actuators across the pupil. The total number of actuators will be this number squared.
+	actuator_spacing : scalar
+		The spacing between actuators before tilting the deformable mirror.
+	x_tilt : scalar
+		The tilt of the deformable mirror around the x-axis in radians.
+	y_tilt : scalar
+		The tilt of the deformable mirror around the y-axis in radians.
+	z_tilt : scalar
+		The tilt of the deformable mirror around the z-axis in radians.
+
+	Returns
+	-------
+	ModeBasis
+		The influence functions for each of the actuators.
+	'''
+	extent = actuator_spacing * (num_actuators_across_pupil - 1)
+	actuator_positions = make_uniform_grid(num_actuators_across_pupil, [extent] * 2)
+
+	evaluated_grid = pupil_grid.scaled(1 / np.cos([y_tilt, x_tilt])).rotated(-z_tilt)
+
+	actuator = np.squeeze(read_fits(pkg_resources.resource_stream('hcipy', 'optics/influence_dm5v2.fits')))
+	actuator_grid = make_uniform_grid(actuator.shape, np.array(actuator.shape) * actuator_spacing / 10.0)
+	actuator = make_linear_interpolator_separated(actuator.ravel(), actuator_grid, 0)
+
+	modes = [actuator(evaluated_grid.shifted(-p)) for p in actuator_positions]
+	modes = [Field(m, pupil_grid) for m in modes]
+	return ModeBasis(modes)
 
 class DeformableMirror(OpticalElement):
 	'''A deformable mirror using influence functions.
@@ -84,4 +126,4 @@ class DeformableMirror(OpticalElement):
 		Field
 			The calculated phase deformation.
 		'''
-		return 2 * self.surface * wavefront.wavenumber
+		return 2 * self.surface * 2*np.pi / wavelength

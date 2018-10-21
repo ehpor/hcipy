23f656b30c63325e6ea4999593dbbfa79a6ad1b5
Emiel Por <por@strw.leidenuniv.nl>
2018-10-21 23:30:35 +0200

    Add fill_value for points outside of the domain of the input field.

--- hcipy/interpolation/linear.py
+++ hcipy/interpolation/linear.py
@@ -2,7 +2,7 @@ from scipy.interpolate import RegularGridInterpolator
 import numpy as np
 from ..field import Field
 
-def make_linear_interpolator_separated(field, grid=None):
+def make_linear_interpolator_separated(field, grid=None, fill_value=None):
 	'''Make a linear interpolators for a separated grid.
 
 	Eventually these functions will be replaced by a single `make_linear_interpolator` function, that
@@ -14,18 +14,23 @@ def make_linear_interpolator_separated(field, grid=None):
 		The field to interpolate.
 	grid : Grid or None
 		The grid of the field. If it is given, the grid of `field` is replaced by this grid.
+	fill_value : scalar
+		The value to use for points outside of the domain of the input field. If this is None, the values
+		outside the domain are extrapolated.
 	
 	Returns
 	-------
 	Field generator
-		The interpolator, as a Field generator. The grid on which this field generator is evaluated, does
+		The interpolator, as a Field generator. The grid on which this field generator will evaluated, does
 		not have to be separated.
 	'''
 	if grid is None:
 		grid = field.grid
+	else:
+		field = Field(field, grid)
 	
 	axes_reversed = np.array(grid.separated_coords)
-	interp = RegularGridInterpolator(axes_reversed, field.shaped, 'linear', False, None)
+	interp = RegularGridInterpolator(axes_reversed, field.shaped, 'linear', False, fill_value)
 
 	def interpolator(evaluated_grid):
 		evaluated_coords = np.flip(np.array(evaluated_grid.coords), 0)

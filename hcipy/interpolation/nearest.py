4c430ad18bdd366f731fa7cc2552f8ec29758ded
Emiel Por <por@strw.leidenuniv.nl>
2018-10-22 01:26:10 +0200

    Add full linear and nearest interpolation.

--- hcipy/interpolation/nearest.py
+++ hcipy/interpolation/nearest.py
@@ -1,9 +1,9 @@
-from scipy.interpolate import RegularGridInterpolator, LinearNDInterpolator
+from scipy.interpolate import RegularGridInterpolator, NearestNDInterpolator
 import numpy as np
 from ..field import Field
 
-def make_linear_interpolator_separated(field, grid=None, fill_value=np.nan):
-	'''Make a linear interpolators for a separated grid.
+def make_nearest_interpolator_separated(field, grid=None):
+	'''Make a nearest interpolator for a field on a separated grid.
 
 	Parameters
 	----------
@@ -11,9 +11,6 @@ def make_linear_interpolator_separated(field, grid=None, fill_value=np.nan):
 		The field to interpolate.
 	grid : Grid or None
 		The grid of the field. If it is given, the grid of `field` is replaced by this grid.
-	fill_value : scalar
-		The value to use for points outside of the domain of the input field. If this is None, the values
-		outside the domain are extrapolated.
 	
 	Returns
 	-------
@@ -27,7 +24,7 @@ def make_linear_interpolator_separated(field, grid=None, fill_value=np.nan):
 		field = Field(field, grid)
 	
 	axes_reversed = np.array(grid.separated_coords)
-	interp = RegularGridInterpolator(axes_reversed, field.shaped, 'linear', False, fill_value)
+	interp = RegularGridInterpolator(axes_reversed, field.shaped, 'nearest', False)
 
 	def interpolator(evaluated_grid):
 		evaluated_coords = np.flip(np.array(evaluated_grid.coords), 0)
@@ -36,8 +33,8 @@ def make_linear_interpolator_separated(field, grid=None, fill_value=np.nan):
 
 	return interpolator
 
-def make_linear_interpolator_unstructured(field, grid=None, fill_value=np.nan):
-	'''Make a linear interpolator for an unstructured grid.
+def make_nearest_interpolator_unstructured(field, grid=None):
+	'''Make a nearest interpolator for an unstructured grid.
 
 	Parameters
 	----------
@@ -45,8 +42,6 @@ def make_linear_interpolator_unstructured(field, grid=None, fill_value=np.nan):
 		The field to interpolate.
 	grid : Grid or None
 		The grid of the field. If it is given, the grid of `field` is replaced by this grid.
-	fill_value : scalar
-		The value to use for points outside of the domain of the input field. Extrapolation is not supported.
 	
 	Returns
 	-------
@@ -54,15 +49,12 @@ def make_linear_interpolator_unstructured(field, grid=None, fill_value=np.nan):
 		The interpolator as a Field generator. The grid on which this field generator will be evaluated does
 		not need to have any structure.
 	'''
-	if fill_value is None:
-		raise ValueError('Extrapolation is not supported for a linear interpolator on an unstructured grid.')
-	
 	if grid is None:
 		grid = field.grid
 	else:
 		field = Field(field, grid)
 	
-	interp = LinearNDInterpolator(grid.points, field, fill_value)
+	interp = NearestNDInterpolator(grid.points, field, fill_value)
 
 	def interpolator(evaluated_grid):
 		res = interp(grid.points)
@@ -70,8 +62,8 @@ def make_linear_interpolator_unstructured(field, grid=None, fill_value=np.nan):
 	
 	return interpolator
 
-def make_linear_interpolator(field, grid=None, fill_value=None):
-	'''Make a linear interpolator for any type of grid.
+def make_nearest_interpolator(field, grid=None):
+	'''Make a nearest interpolator for any type of grid.
 
 	Parameters
 	----------
@@ -79,9 +71,6 @@ def make_linear_interpolator(field, grid=None, fill_value=None):
 		The field to interpolate.
 	grid : Grid or None
 		The grid of the field. If it is given, the grid of `field` is replaced by this grid.
-	fill_value : scalar or None
-		The value to use for points outside of the domain of the input field. Extrapolation is not supported.
-		If it is None, a numpy.nan value will be used for points outside of the domain.
 	
 	Returns
 	-------
@@ -93,6 +82,6 @@ def make_linear_interpolator(field, grid=None, fill_value=None):
 		grid = field.grid
 	
 	if grid.is_unstructured:
-		return make_linear_interpolator_unstructured(field, grid)
+		return make_nearest_interpolator_unstructured(field, grid)
 	else:
-		return make_linear_interpolator_separated(field, grid)
+		return make_nearest_interpolator_separated(field, grid)

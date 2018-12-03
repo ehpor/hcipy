fe5e94af109df8b07686d9f690383fee1b2d86bf
Emiel Por <por@strw.leidenuniv.nl>
2018-12-03 22:09:21 +0100

    Split read_mode_basis test into multiple separated tests.

--- tests/test_io.py
+++ tests/test_io.py
@@ -3,116 +3,114 @@ import os
 from hcipy import *
 
 def test_write_mode_basis():
-    # grid for the test mode basis
-    pupil_grid = make_pupil_grid(128)
+	# grid for the test mode basis
+	pupil_grid = make_pupil_grid(128)
 
-    # generating a test mode basis 
-    test_mode_basis = make_zernike_basis(num_modes=20, D=1, grid=pupil_grid, starting_mode=1, ansi=False, radial_cutoff=True)
+	# generating a test mode basis 
+	test_mode_basis = make_zernike_basis(num_modes=20, D=1, grid=pupil_grid, starting_mode=1, ansi=False, radial_cutoff=True)
 
-    # turning it into an array so we can compare 
-    test_mode_basis_array = np.array([test_mode_basis])
-    test_mode_basis_array = np.reshape(test_mode_basis_array, [20,128,128])
+	file_name = 'write_mode_basis_test.fits'
 
-    file_name = 'write_mode_basis_test.fits'
+	# saving the mode basis 
+	write_mode_basis(test_mode_basis, file_name)
 
-    # saving the mode basis 
-    write_mode_basis(test_mode_basis, file_name)
+	# loading it
+	test_mode_basis_array_read = read_fits(file_name)
 
-    # loading it very simply 
-    test_mode_basis_array_read = read_fits(file_name)
+	# comparing the arrays
+	test_mode_basis_array = np.array([test_mode_basis])
+	test_mode_basis_array = np.reshape(test_mode_basis_array, [20,128,128])
+	assert np.isclose(test_mode_basis_array, test_mode_basis_array_read, rtol=1e-02, atol=1e-05).all() 
 
-    # comparing the arrays
-    assert np.isclose(test_mode_basis_array, test_mode_basis_array_read, rtol=1e-02, atol=1e-05).all() 
+	# Remove temporary file.
+	os.remove(file_name)
 
-    os.remove(file_name)
+def test_read_mode_basis_1():
+	#-------------------------------
+	# testing a square mode basis that we read without providing a grid
+	#-------------------------------
 
-def test_read_mode_basis():
-    #-------------------------------
-    # testing a square mode basis that we read without providing a grid
-    #-------------------------------
+	# grid for the test mode basis
+	pupil_grid = make_pupil_grid(128)
 
-    # grid for the test mode basis
-    pupil_grid_1 = make_pupil_grid(128)
+	# testing a square mode basis defined in the pupil plane 
+	test_mode_basis = make_zernike_basis(num_modes=20, D=1, grid=pupil_grid, starting_mode=1, ansi=False, radial_cutoff=True)
 
-    # testing a square mode basis defined in the pupil plane 
-    test_mode_basis_1 = make_zernike_basis(num_modes=20, D=1, grid=pupil_grid_1, starting_mode=1, ansi=False, radial_cutoff=True)
+	# writing the mode basis 
+	file_name = 'read_mode_basis_test_1.fits'
+	write_mode_basis(test_mode_basis, file_name)
 
-    file_name_1 = 'read_mode_basis_test_1.fits'
+	# and loading it again 
+	test_mode_basis_read = read_mode_basis(file_name, grid=None)
 
-    # writing the mode basis 
-    write_mode_basis(test_mode_basis_1, file_name_1)
+	# checking if the modes are still the same 
+	for mode, mode_read in zip(test_mode_basis, test_mode_basis_read):
+		assert np.isclose(mode, mode_read, rtol=1e-02, atol=1e-05).all()     
 
-    # and loading it again 
-    test_mode_basis_1_read = read_mode_basis(file_name_1, grid=None)
+	# checking if the grid is correct 
+	assert np.isclose(pupil_grid.x, test_mode_basis_read[0].grid.x, rtol=1e-02, atol=1e-05).all()     
+	assert np.isclose(pupil_grid.y, test_mode_basis_read[0].grid.y, rtol=1e-02, atol=1e-05).all()
 
-    # checking if the modes are still the same 
-    for mode, mode_read in zip(test_mode_basis_1, test_mode_basis_1_read):
-        assert np.isclose(mode, mode_read, rtol=1e-02, atol=1e-05).all()     
+	# Remove temporary file.
+	os.remove(file_name)
 
-    # checking if the grid is correct 
-    assert np.isclose(pupil_grid_1.x, test_mode_basis_1_read[0].grid.x, rtol=1e-02, atol=1e-05).all()     
-    assert np.isclose(pupil_grid_1.y, test_mode_basis_1_read[0].grid.y, rtol=1e-02, atol=1e-05).all()     
+def test_read_mode_basis_2():
+	#-------------------------------
+	# testing a square mode basis that we read with providing a grid
+	#-------------------------------
 
-    os.remove(file_name_1)
+	# grid for the test mode basis
+	pupil_grid = make_pupil_grid(128, 3)
 
-    #-------------------------------
-    # testing a square mode basis that we read with providing a grid
-    #-------------------------------
+	# testing a square mode basis defined in the pupil plane 
+	test_mode_basis = make_zernike_basis(num_modes=20, D=3, grid=pupil_grid, starting_mode=1, ansi=False, radial_cutoff=True)
 
-    # grid for the test mode basis
-    pupil_grid_2 = make_pupil_grid(128, 3)
+	# writing the mode basis 
+	file_name = 'read_mode_basis_test_2.fits'
+	write_mode_basis(test_mode_basis, file_name)
 
-    # testing a square mode basis defined in the pupil plane 
-    test_mode_basis_2 = make_zernike_basis(num_modes=20, D=3, grid=pupil_grid_2, starting_mode=1, ansi=False, radial_cutoff=True)
+	# and loading it again 
+	test_mode_basis_read = read_mode_basis(file_name, grid=pupil_grid)
 
-    file_name_2 = 'read_mode_basis_test_2.fits'
+	# checking if the modes are still the same 
+	for mode, mode_read in zip(test_mode_basis, test_mode_basis_read):
+		assert np.isclose(mode, mode_read, rtol=1e-02, atol=1e-05).all()     
 
-    # writing the mode basis 
-    write_mode_basis(test_mode_basis_2, file_name_2)
+	# checking if the grid is correct 
+	assert np.isclose(pupil_grid.x, test_mode_basis_read[0].grid.x, rtol=1e-02, atol=1e-05).all()     
+	assert np.isclose(pupil_grid.y, test_mode_basis_read[0].grid.y, rtol=1e-02, atol=1e-05).all()     
 
-    # and loading it again 
-    test_mode_basis_2_read = read_mode_basis(file_name_2, grid=pupil_grid_2)
+	# Remove temporary file.
+	os.remove(file_name)
 
-    # checking if the modes are still the same 
-    for mode, mode_read in zip(test_mode_basis_2, test_mode_basis_2_read):
-        assert np.isclose(mode, mode_read, rtol=1e-02, atol=1e-05).all()     
+def test_read_mode_basis_3():
+	#-------------------------------
+	# testing a non-square mode basis that we read with providing a grid
+	#-------------------------------
 
-    # checking if the grid is correct 
-    assert np.isclose(pupil_grid_2.x, test_mode_basis_2_read[0].grid.x, rtol=1e-02, atol=1e-05).all()     
-    assert np.isclose(pupil_grid_2.y, test_mode_basis_2_read[0].grid.y, rtol=1e-02, atol=1e-05).all()     
+	# grid for the test mode basis
+	pupil_grid = make_uniform_grid([128,256], [128,256], center=0, has_center=False)
 
-    os.remove(file_name_2)
+	# testing a square mode basis defined in the pupil plane 
+	test_mode_basis = []
+	for i in np.arange(20):
+		test_mode_basis.append(Field(np.random.rand(128*256), pupil_grid))
+	test_mode_basis = ModeBasis(test_mode_basis)
 
+	# writing the mode basis
+	file_name = 'read_mode_basis_test_3.fits'
+	write_mode_basis(test_mode_basis, file_name)
 
-    #-------------------------------
-    # testing a non-square mode basis that we read with providing a grid
-    #-------------------------------
+	# and loading it again 
+	test_mode_basis_read = read_mode_basis(file_name, grid=pupil_grid)
 
-    # grid for the test mode basis
-    pupil_grid_3 = make_uniform_grid([128,256], [128,256], center=0, has_center=False)
+	# checking if the modes are still the same 
+	for mode, mode_read in zip(test_mode_basis, test_mode_basis_read):
+		assert np.isclose(mode, mode_read, rtol=1e-02, atol=1e-05).all()     
 
-    # testing a square mode basis defined in the pupil plane 
-    test_mode_basis_3 = []
+	# checking if the grid is correct 
+	assert np.isclose(pupil_grid.x, test_mode_basis_read[0].grid.x, rtol=1e-02, atol=1e-05).all()     
+	assert np.isclose(pupil_grid.y, test_mode_basis_read[0].grid.y, rtol=1e-02, atol=1e-05).all()     
 
-    for i in np.arange(20):
-        test_mode_basis_3.append(Field(np.random.rand(128*256), pupil_grid_3))
-
-    test_mode_basis_3 = ModeBasis(test_mode_basis_3)
-
-    file_name_3 = 'read_mode_basis_test_3.fits'
-
-    # writing the mode basis 
-    write_mode_basis(test_mode_basis_3, file_name_3)
-
-    # and loading it again 
-    test_mode_basis_3_read = read_mode_basis(file_name_3, grid=pupil_grid_3)
-
-    # checking if the modes are still the same 
-    for mode, mode_read in zip(test_mode_basis_3, test_mode_basis_3_read):
-        assert np.isclose(mode, mode_read, rtol=1e-02, atol=1e-05).all()     
-
-    # checking if the grid is correct 
-    assert np.isclose(pupil_grid_3.x, test_mode_basis_3_read[0].grid.x, rtol=1e-02, atol=1e-05).all()     
-    assert np.isclose(pupil_grid_3.y, test_mode_basis_3_read[0].grid.y, rtol=1e-02, atol=1e-05).all()     
-
-    os.remove(file_name_3)
+	# Remove temporary file.
+	os.remove(file_name)

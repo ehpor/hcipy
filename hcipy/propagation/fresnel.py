b30471c7191aa756fa765ec409e1a0b1a887c727
Sebastiaan Haffert <haffert@strw.leidenuniv.nl>
2019-03-25 22:18:02 +0100

    added a refractive index for propagation inside medium

--- hcipy/propagation/fresnel.py
+++ hcipy/propagation/fresnel.py
@@ -5,13 +5,13 @@ from ..field import Field
 from ..fourier import FastFourierTransform
 
 class FresnelPropagatorMonochromatic(object):
-	def __init__(self, input_grid, distance, wavelength=1):
+	def __init__(self, input_grid, distance, wavelength=1, refractive_index=1):
 		self.fft = FastFourierTransform(input_grid)
 		
-		k = 2*np.pi / wavelength
+		k = 2*np.pi / wavelength * refractive_index
 		k_squared = self.fft.output_grid.as_('polar').r**2
 		phase_factor = np.exp(1j * k * distance)
-		self.transfer_function = np.exp(-0.5j * distance * k_squared / k)# * phase_factor
+		self.transfer_function = np.exp(-0.5j * distance * k_squared / k) * phase_factor
 	
 	def forward(self, wavefront):
 		ft = self.fft.forward(wavefront.electric_field)

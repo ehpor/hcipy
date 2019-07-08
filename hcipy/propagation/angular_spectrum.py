4f692f4dafcf876beca725126ad42c3afaf9be75
Sebastiaan Haffert <haffert@strw.leidenuniv.nl>
2019-07-08 14:44:38 +0200

    added documentation

--- hcipy/propagation/angular_spectrum.py
+++ hcipy/propagation/angular_spectrum.py
@@ -7,14 +7,24 @@ from ..plotting import imshow_field
 from matplotlib import pyplot as plt
 
 class AngularSpectrumPropagatorMonochromatic(object):
-	'''The monochromatic propagator for scalar fields.
+	'''The monochromatic angular spectrum propagator for scalar fields.
+
+	The scalar Angular Spectrum propagator is implemented as described by
+	 "Vector Fourier optics of anisotropicmaterials", Mcleod 2014. The propagation
+	 of an electric field can be described as a transfer function in frequency space.
+	 The transfer function is taken from equation 9 of Mcleod 2014, and the related
+	 impulse response is taken from equation 6.
 
 	Parameters
 	----------
-		
-	Attributes
-	----------
-	
+	input_grid : Grid
+		The grid on which the incoming wavefront is defined.
+	distance : scalar
+		The distance to propagate
+	wavelength : scalar
+		The wavelength of the wavefront.
+	refractive_index : scalar
+		The refractive index of the medium that the wavefront is propagating in.
 	'''
 	def __init__(self, input_grid, distance, wavelength=1, refractive_index=1):
 
@@ -25,15 +35,10 @@ class AngularSpectrumPropagatorMonochromatic(object):
 		# The FFT requires a regular cartesian grid there we can use the cartesian coordinates.
 		Lmax = np.max([coord.ptp() for coord in input_grid.coords])
 		
-		# This condition is violated we have a phase that varies to much
-		# and we need to sample the transfer function in real space.
 		if np.any(input_grid.delta > wavelength * distance / Lmax):
-			
-			# Distance from the origin to the evaluation point
+
 			r = np.sqrt(input_grid.as_('polar').r**2 + distance**2)
-			# Angle of the evaluation point w.r.t. propagation direction
 			cos_theta = distance/r
-			# Evaluate the impulse response equation 6 of "Vector Fourier optics of anisotropicmaterials", Mcleod 2014.
 			impulse_response = cos_theta/(2*np.pi) * np.exp(1j * k * r) * (1/r**2 - 1j*k/r)
 			
 			self.transfer_function = self.fft.forward(impulse_response)
@@ -43,11 +48,35 @@ class AngularSpectrumPropagatorMonochromatic(object):
 			self.transfer_function = np.exp(1j * k_z * distance)
 
 	def forward(self, wavefront):
+		'''Propagate a wavefront forward by a certain distance.
+	
+		Parameters
+		----------
+		wavefront : Wavefront
+			The incoming wavefront.
+		
+		Returns
+		-------
+		Wavefront
+			The wavefront after the propagation.
+		'''
 		ft = self.fft.forward(wavefront.electric_field)
 		ft *= self.transfer_function
 		return Wavefront(self.fft.backward(ft), wavefront.wavelength)
 	
 	def backward(self, wavefront):
+		'''Propagate a wavefront backward by a certain distance.
+	
+		Parameters
+		----------
+		wavefront : Wavefront
+			The incoming wavefront.
+		
+		Returns
+		-------
+		Wavefront
+			The wavefront after the propagation.
+		'''
 		ft = self.fft.forward(wavefront.electric_field)
 		ft *= np.conj(self.transfer_function)
 		return Wavefront(self.fft.backward(ft), wavefront.wavelength)
--- hcipy/propagation/fresnel.py
+++ hcipy/propagation/fresnel.py
@@ -5,6 +5,21 @@ from ..field import Field
 from ..fourier import FastFourierTransform
 
 class FresnelPropagatorMonochromatic(object):
+	'''The monochromatic Fresnel propagator for scalar fields.
+
+	The Fresnel propagator is implemented as described in "Fourier Optics" by J. W. Goodman.
+
+	Parameters
+	----------
+	input_grid : Grid
+		The grid on which the incoming wavefront is defined.
+	distance : scalar
+		The distance to propagate
+	wavelength : scalar
+		The wavelength of the wavefront.
+	refractive_index : scalar
+		The refractive index of the medium that the wavefront is propagating in.
+	'''
 	def __init__(self, input_grid, distance, wavelength=1, refractive_index=1):
 		self.fft = FastFourierTransform(input_grid)
 		
@@ -14,13 +29,37 @@ class FresnelPropagatorMonochromatic(object):
 		self.transfer_function = np.exp(-0.5j * distance * k_squared / k) * phase_factor
 	
 	def forward(self, wavefront):
+		'''Propagate a wavefront forward by a certain distance.
+	
+		Parameters
+		----------
+		wavefront : Wavefront
+			The incoming wavefront.
+		
+		Returns
+		-------
+		Wavefront
+			The wavefront after the propagation.
+		'''
 		ft = self.fft.forward(wavefront.electric_field)
 		ft *= self.transfer_function
 		return Wavefront(self.fft.backward(ft), wavefront.wavelength)
 	
 	def backward(self, wavefront):
+		'''Propagate a wavefront forward by a certain distance.
+	
+		Parameters
+		----------
+		wavefront : Wavefront
+			The incoming wavefront.
+		
+		Returns
+		-------
+		Wavefront
+			The wavefront after the propagation.
+		'''
 		ft = self.fft.forward(wavefront.electric_field)
-		ft /= self.transfer_function
+		ft *= np.conj(self.transfer_function)
 		return Wavefront(self.fft.backward(ft), wavefront.wavelength)
 
 FresnelPropagator = make_polychromatic(["refractive_index"])(FresnelPropagatorMonochromatic)

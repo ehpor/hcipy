091e2c9ed341c50f8cb1920c272bf65c84eb0b63
Emiel Por <por@strw.leidenuniv.nl>
2019-07-08 12:20:06 +0200

    Remove encoding parameter for Python2 compliance.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # read the contents of the README.md file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
 	long_description = f.read()
 
 setup(name='hcipy',

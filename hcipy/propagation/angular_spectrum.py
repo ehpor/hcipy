0924d89c228ff5d470904816aa522a66e868383e
haffert <haffert@strw.leidenuniv.nl>
2019-07-11 14:43:37 +0200

    Adjusted the ASP and Fresnel to include oversampling of TF.

--- hcipy/propagation/angular_spectrum.py
+++ hcipy/propagation/angular_spectrum.py
@@ -21,6 +21,8 @@ class AngularSpectrumPropagatorMonochromatic(object):
 		The grid on which the incoming wavefront is defined.
 	distance : scalar
 		The distance to propagate
+	num_oversampling : int
+		The number of times the transfer function is oversampled. Default is 2.
 	wavelength : scalar
 		The wavelength of the wavefront.
 	refractive_index : scalar
@@ -31,7 +33,7 @@ class AngularSpectrumPropagatorMonochromatic(object):
 	ValueError
 		If the `input_grid` is not regular and Cartesian.
 	'''
-	def __init__(self, input_grid, distance, wavelength=1, refractive_index=1):
+	def __init__(self, input_grid, distance, num_oversampling=2, wavelength=1, refractive_index=1):
 		# The FFT requires a regular cartesian grid
 		if not input_grid.is_regular or not input_grid.is_('cartesian'):
 			raise ValueError('The input grid must be a regular, Cartesian grid.')
@@ -41,17 +43,26 @@ class AngularSpectrumPropagatorMonochromatic(object):
 		k = 2 * np.pi / wavelength * refractive_index
 		L_max = np.max(input_grid.dims * input_grid.delta)
 		
-		if np.any(input_grid.delta < (wavelength * distance / L_max)):
-			r_squared = input_grid.x**2 + input_grid.y**2 + distance**2
-			r = np.sqrt(r_squared)
-			
-			cos_theta = distance / r
-			impulse_response = Field(cos_theta / (2 * np.pi) * np.exp(1j * k * r) * (1 / r_squared - 1j * k / r), input_grid)
-			self.transfer_function = self.fft.forward(impulse_response)
+		if np.any(input_grid.delta < wavelength * distance / L_max):			
+			enlarged_input_grid = make_pupil_grid(2 * input_grid.dims, 2 * input_grid.delta * (input_grid.dims-1) )
+			self.fft_up_scale = FastFourierTransform(enlarged_input_grid)
+
+			def impulse_response_generator(grid):
+				r_squared = grid.x**2 + grid.y**2
+				r = np.sqrt(r_squared)
+				cos_theta = distance / r
+				return Field(cos_theta / (2 * np.pi) * np.exp(1j * k * r) * (1 / r_squared - 1j * k / r), grid)
+
+			impulse_response = evaluate_supersampled(impulse_response_generator, enlarged_input_grid, num_oversampling)
+
+			self.transfer_function = self.fft_up_scale.forward(impulse_response)
 		else:
-			k_squared = self.fft.output_grid.as_('polar').r**2
-			k_z = np.sqrt(k**2 - k_squared + 0j)
-			self.transfer_function = np.exp(1j * k_z * distance)
+			def transfer_function_generator(grid):
+				k_squared = grid.as_('polar').r**2
+				k_z = np.sqrt(k**2 - k_squared + 0j)
+				return Fuekd(np.exp(1j * k_z * distance), grid)
+
+			self.transfer_function = evaluate_supersampled(transfer_function_generator, self.fft.output_grid, num_oversampling)
 
 	def forward(self, wavefront):
 		'''Propagate a wavefront forward by a certain distance.
--- hcipy/propagation/fresnel.py
+++ hcipy/propagation/fresnel.py
@@ -3,6 +3,10 @@ from .propagator import MonochromaticPropagator
 from ..optics import Wavefront, make_polychromatic
 from ..field import Field
 from ..fourier import FastFourierTransform
+from matplotlib import pyplot as plt
+from ..plotting import imshow_field
+from ..field import evaluate_supersampled, make_pupil_grid, subsample_field
+from ..aperture import circular_aperture
 
 class FresnelPropagatorMonochromatic(object):
 	'''The monochromatic Fresnel propagator for scalar fields.
@@ -17,6 +21,8 @@ class FresnelPropagatorMonochromatic(object):
 		The grid on which the incoming wavefront is defined.
 	distance : scalar
 		The distance to propagate
+	num_oversampling : int
+		The number of times the transfer function is oversampled. Default is 2.
 	wavelength : scalar
 		The wavelength of the wavefront.
 	refractive_index : scalar
@@ -27,25 +33,35 @@ class FresnelPropagatorMonochromatic(object):
 	ValueError
 		If the `input_grid` is not regular and Cartesian.
 	'''
-	def __init__(self, input_grid, distance, wavelength=1, refractive_index=1):
+	def __init__(self, input_grid, distance, num_oversampling=2, wavelength=1, refractive_index=1):
 		# The FFT requires a regular cartesian grid
 		if not input_grid.is_regular or not input_grid.is_('cartesian'):
 			raise ValueError('The input grid must be a regular, Cartesian grid.')
 		
-		self.fft = FastFourierTransform(input_grid)
+		self.fft = FastFourierTransform(input_grid, q=2)
 		
 		k = 2 * np.pi / wavelength * refractive_index
 		L_max = np.max(input_grid.dims * input_grid.delta)
 		
-		if np.any(input_grid.delta < wavelength * distance / L_max):
-			r_squared = input_grid.x**2 + input_grid.y**2
-			impulse_response = Field(np.exp(1j * k * distance) / (1j * wavelength * distance) * np.exp(1j * k * r_squared / (2 * distance)), input_grid)
-			self.transfer_function = self.fft.forward(impulse_response)
+		if np.any(input_grid.delta < wavelength * distance / L_max):			
+			enlarged_input_grid = make_pupil_grid(2 * input_grid.dims, 2 * input_grid.delta * (input_grid.dims-1) )
+			self.fft_up_scale = FastFourierTransform(enlarged_input_grid)
+
+			def impulse_response_generator(grid):
+				r_squared = grid.x**2 + grid.y**2
+				return Field(np.exp(1j * k * distance) / (1j * wavelength * distance) * np.exp(1j * k * r_squared / (2 * distance)), grid)
+
+			impulse_response = evaluate_supersampled(impulse_response_generator, enlarged_input_grid, num_oversampling)
+
+			self.transfer_function = self.fft_up_scale.forward(impulse_response)
 		else:
-			k_squared = self.fft.output_grid.as_('polar').r**2
-			phase_factor = np.exp(1j * k * distance)
-			self.transfer_function = np.exp(-0.5j * distance * k_squared / k) * phase_factor
-	
+			def transfer_function_generator(grid):
+				k_squared = grid.as_('polar').r**2
+				phase_factor = np.exp(1j * k * distance)
+				return Field(np.exp(-0.5j * distance * k_squared / k) * phase_factor, grid)
+
+			self.transfer_function = evaluate_supersampled(transfer_function_generator, self.fft.output_grid, num_oversampling)
+
 	def forward(self, wavefront):
 		'''Propagate a wavefront forward by a certain distance.
 	

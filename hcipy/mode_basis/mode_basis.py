95025538fa1d67ad694263ffcda24cef805fdf71
Emiel Por <por@strw.leidenuniv.nl>
2019-07-17 03:15:41 +0200

    Remove old code.

--- hcipy/mode_basis/mode_basis.py
+++ hcipy/mode_basis/mode_basis.py
@@ -2,23 +2,7 @@ import numpy as np
 import scipy.sparse
 
 from ..field import Field
-'''
-class ModeBasis(list):
-	@property
-	def transformation_matrix(self):
-		return np.stack(self, axis=-1)
-	
-	# TODO: currently only works on scalar fields
-	@property
-	def orthogonalized(self):
-		q, r = np.linalg.qr(self.transformation_matrix)
-		if hasattr(self[0], 'grid'):
-			from ..field import Field
-			grid = self[0].grid
-			return ModeBasis((Field(q[:,i], grid) for i in range(q.shape[1])))
-		else:
-			return ModeBasis((q[:,i] for i in range(q.shape[1])))
-'''
+
 class ModeBasis(object):
 	'''A list of modes.
 

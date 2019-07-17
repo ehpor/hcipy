d8a3363d92e160f8b83ca82e00fee841276fadfe
Emiel Por <por@strw.leidenuniv.nl>
2019-07-17 03:15:30 +0200

    Add documentation.

--- hcipy/math_util/singular_value_decomposition.py
+++ hcipy/math_util/singular_value_decomposition.py
@@ -1,6 +1,20 @@
 import numpy as np
 
 class SVD(object):
+	'''The Singular Value Decomposition for the provided matrix.
+
+	This class wraps two versions of the SVD in numpy and scipy, and provides
+	easy access to singular modes (as mode bases) and allows for calculation
+	of the SVD for a limited number of modes.
+
+	Parameters
+	----------
+	M : np.ndarray
+		The matrix on which to perform the SVD.
+	num_modes : int or None
+		The number of singular values and modes to calculate. If this is None,
+		all modes will be computed.
+	'''
 	def __init__(self, M, num_modes=None):
 		self._M = M
 		self._num_modes = num_modes
@@ -14,45 +28,63 @@ class SVD(object):
 	
 	@property
 	def left_singular_modes(self):
+		'''The left singular modes of the matrix, as a ModeBasis.
+		'''
 		from ..mode_basis import ModeBasis
 		
 		return ModeBasis((m for m in self.U.conj().T))
 	
 	@property
 	def right_singular_modes(self):
+		'''The right singular modes of the matrix, as a ModeBasis.
+		'''
 		from ..mode_basis import ModeBasis
 
 		return ModeBasis((m for m in self.Vt.conj()))
 	
 	@property
 	def singular_values(self):
+		'''The singular values of the matrix.
+		'''
 		return self.S
 	
 	@property
 	def U(self):
+		'''The U matrix of the SVD.
+		'''
 		return self.svd[0]
 	
 	@property
 	def S(self):
+		'''The singular values of the matrix.
+		'''
 		return self.svd[1]
 	
 	@property
 	def Vt(self):
+		'''The V^T matrix of the SVD.
+		'''
 		return self.svd[2]
 	
 	def __getitem__(self, i):
+		'''The raw U, S, and V^T matrices of the SVD by index.
+		'''
 		return self.svd[i]
 	
 	@property
 	def svd(self):
+		'''The raw U, S, and V^T matrices of the SVD as a tuple.
+		'''
 		return self._svd
 	
 	@property
 	def num_modes(self):
+		'''The number of singular modes that were calculated in this SVD.
+		'''
 		return self._num_modes
 	
 	@property
 	def M(self):
+		'''The matrix for which the SVD was calculated.
+		'''
 		return self._M
-
-# Incremental SVD planned
--- hcipy/optics/gaussian_beam.py
+++ hcipy/optics/gaussian_beam.py
@@ -1,6 +1,19 @@
 import numpy as np
+from ..field import Field
+from .wavefront import Wavefront
 
 class GaussianBeam(object):
+	'''An analytical description of a light beam with a Gaussian profile.
+
+	Parameters
+	----------
+	w0 : scalar
+		The beam waist.
+	z : scalar
+		The current distance from the beam waist.
+	wavelength : scalar
+		The wavelength of the light.
+	'''
 	def __init__(self, w0, z, wavelength):
 		self.w0 = w0
 		self.z = z
@@ -8,6 +21,8 @@ class GaussianBeam(object):
 	
 	@property
 	def beam_waist(self):
+		'''The beam waist of the Gaussian beam.
+		'''
 		return self.w0
 	
 	@beam_waist.setter
@@ -16,6 +31,8 @@ class GaussianBeam(object):
 
 	@property
 	def zR(self):
+		'''The Rayleight distance of the Gaussian beam.
+		'''
 		return np.pi * self.w0**2 / self.wavelength
 	
 	@zR.setter
@@ -26,6 +43,8 @@ class GaussianBeam(object):
 
 	@property
 	def q(self):
+		'''The complex beam parameter of the Gaussian beam.
+		'''
 		return self.z + 1j * self.zR
 	
 	@q.setter
@@ -37,6 +56,8 @@ class GaussianBeam(object):
 
 	@property
 	def theta(self):
+		'''The beam divergence of the Gaussian beam.
+		'''
 		return self.wavelength / (np.pi * self.w0)
 	
 	@theta.setter
@@ -47,39 +68,62 @@ class GaussianBeam(object):
 
 	@property
 	def R(self):
+		'''The current radius of curvature of the Gaussian beam.
+		'''
 		return self.z * (1 + (self.zR / self.z)**2)
 	
 	radius_of_curvature = R
 
 	@property
 	def psi(self):
+		'''The current Gouy phase of the Gaussian beam.
+		'''
 		return np.arctan(self.z / self.zR)
 	
 	gouy_phase = psi
 
 	@property
 	def w(self):
+		'''The current beam radius of the Gaussian beam.
+		'''
 		return self.w0 * np.sqrt(1 + (self.z / self.zR)**2)
 
 	beam_radius = w
 
 	@property
 	def FWHM(self):
+		'''The current FWHM of the Gaussian beam.
+		'''
 		return self.w * np.sqrt(2 * np.log(2))
 	
 	full_width_half_maximum = FWHM
 
 	@property
 	def k(self):
+		'''The wavenumber of the Gaussian beam.
+		'''
 		return 2 * np.pi / self.wavelength
 	
 	@k.setter
 	def k(self, k):
 		self.wavelength = 2 * np.pi / k
 	
-	wave_number = k
+	wavenumber = k
 	
 	def evaluate(self, grid):
+		'''Evaluate the wavefront of the Gaussian beam at the current position on 
+		the given grid.
+
+		Parameters
+		----------
+		grid : Grid
+			The grid on which to calculate the wavefront for the Gaussian beam.
+		
+		Returns
+		-------
+		Wavefront
+			The evaluated wavefront of the Gaussian beam.
+		'''
 		if grid.is_('cartesian'):
 			r2 = grid.x**2 + grid.y**2
 		else:
@@ -89,6 +133,6 @@ class GaussianBeam(object):
 		K2 = np.exp(-r2 / self.w**2)
 		K3 = np.exp(-1j * (self.k * self.z + self.k * r2 / (2 * self.R) - self.psi))
 
-		return Field(K1 * K2 * K3, grid)
+		return Wavefront(Field(K1 * K2 * K3, grid), self.wavelength)
 	
 	__call__ = evaluate

cbd4ec7bddc53b00c795246eafd90f7a45829732
Emiel Por <por@strw.leidenuniv.nl>
2019-08-01 14:44:07 +0200

    Add a knife-edge Lyot coronagraph.

--- hcipy/coronagraphy/__init__.py
+++ hcipy/coronagraphy/__init__.py
@@ -1,10 +1,12 @@
 __all__ = ['generate_app_keller', 'generate_app_por']
+__all__ += ['KnifeEdgeLyotCoronagraph']
 __all__ += ['LyotCoronagraph', 'OccultedLyotCoronagraph']
 __all__ += ['PerfectCoronagraph']
 __all__ += []
 __all__ += ['VortexCoronagraph', 'make_ravc_masks', 'get_ravc_planet_transmission']
 
 from .apodizing_phase_plate import *
+from .knife_edge import *
 from .lyot import *
 from .perfect_coronagraph import *
 from .shaped_pupil import *

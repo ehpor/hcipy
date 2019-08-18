dd31165cd0e3cf80cc8185007f0c024188997a0b
Emiel Por <por@strw.leidenuniv.nl>
2019-08-18 17:17:26 +0200

    Add documentation and convenience functions.

--- hcipy/optics/wavefront.py
+++ hcipy/optics/wavefront.py
@@ -6,6 +6,29 @@ from ..field import Field, field_dot
 # TODO Should add a pilot Gaussian beam with each Wavefront
 
 class Wavefront(object):
+	'''A physical wavefront in an optical system.
+
+	This represents the state of light to be propagated through the
+	optical system. It can be both an electric field in the scalar 
+	approximation (ie. scalar wavefront propgation), a fully polarized
+	wavefront, represented by a Field of Jones vectors, and a potentially
+	partially-polarized wavefront, represented by two Jones vector fields 
+	and the Stokes vector corresponding to the Jones vectors (1,0) and (0,1).
+
+	Parameters
+	----------
+	electric_field : Field
+		The electric field, either scalar, vectorial or 2-tensorial.
+	wavelength : scalar
+		The wavelength of the wavefront.
+	stokes_vector : ndarray or None
+		If a Stokes vector (I, Q, U, V) is given, a partially-polarized
+		wavefront is initialized. If `electric_field` is scalar, it will
+		be transformed into a tensor field with the correct Jones states.
+		If a tensor-field is given as the `electric_field`, the Stokes
+		vector will be interpreted as corresponding to the Jones vectors
+		(1,0) and (0,1).
+	'''
 	def __init__(self, electric_field, wavelength=1, stokes_vector=None):
 		if stokes_vector is not None:
 			if electric_field.tensor_order not in [0, 2]:
@@ -24,10 +47,14 @@ class Wavefront(object):
 		self.wavelength = wavelength
 	
 	def copy(self):
+		'''Make a copy of the wavefront.
+		'''
 		return copy.deepcopy(self)
 	
 	@property
 	def electric_field(self):
+		'''The electric field as function of 2D position on the plane.
+		'''
 		return self._electric_field
 	
 	@electric_field.setter
@@ -42,10 +69,14 @@ class Wavefront(object):
 	
 	@property
 	def input_stokes_vector(self):
+		'''The Stokes vector corresponding to the Jones vectors (1,0) and (0,1).
+		'''
 		return self._input_stokes_vector
 
 	@property
 	def wavenumber(self):
+		'''The wavenumber of the light.
+		'''
 		return 2 * np.pi / self.wavelength
 	
 	@wavenumber.setter
@@ -54,10 +85,15 @@ class Wavefront(object):
 	
 	@property
 	def grid(self):
+		'''The grid on which the electric field is defined.
+		'''
 		return self.electric_field.grid
 
 	@property
 	def I(self):
+		'''The I-component of the Stokes vector as function of 2D position
+		in the plane.
+		'''
 		x = self.electric_field[0, 0, :]
 		y = self.electric_field[0, 1, :]
 		z = self.electric_field[1, 0, :]
@@ -74,6 +110,9 @@ class Wavefront(object):
 
 	@property
 	def Q(self):
+		'''The Q-component of the Stokes vector as function of 2D position
+		in the plane.
+		'''
 		x = self.electric_field[0, 0, :]
 		y = self.electric_field[0, 1, :]
 		z = self.electric_field[1, 0, :]
@@ -90,6 +129,9 @@ class Wavefront(object):
 	
 	@property
 	def U(self):
+		'''The U-component of the Stokes vector as function of 2D position
+		in the plane.
+		'''
 		x = self.electric_field[0, 0, :]
 		y = self.electric_field[0, 1, :]
 		z = self.electric_field[1, 0, :]
@@ -106,6 +148,9 @@ class Wavefront(object):
 
 	@property
 	def V(self):
+		'''The V-component of the Stokes vector as function of 2D position
+		in the plane.
+		'''
 		x = self.electric_field[0, 0, :]
 		y = self.electric_field[0, 1, :]
 		z = self.electric_field[1, 0, :]
@@ -122,35 +167,88 @@ class Wavefront(object):
 	
 	@property
 	def degree_of_polarization(self):
+		'''The degree of polarization.
+		'''
 		return np.sqrt(self.Q**2 + self.U**2 + self.V**2) / self.I
+	
+	@property
+	def degree_of_linear_polarization(self):
+		'''The degree of linear polarization.
+		'''
+		return np.sqrt(self.Q**2 + self.U**2) / self.I
+	
+	@property
+	def degree_of_circular_polarization(self):
+		'''The degree of circular polarization.
+		'''
+		return self.V / self.I
+	
+	@property
+	def ellipticity(self):
+		'''The ratio of the minor to major axis of the electric
+		field polarization ellipse.
+		'''
+		return self.V / (self.I + np.sqrt(self.Q**2 + self.U**2))
+	
+	@property
+	def is_polarized(self):
+		'''If the wavefront can be polarized.
+		'''
+		return self.electric_field.tensor_order in [1, 2]
+	
+	@property
+	def is_partially_polarized(self):
+		'''If the wavefront can be partially polarized.
+		'''
+		return self.electric_field.tensor_order == 2
+	
+	@property
+	def is_scalar(self):
+		'''If the wavefront uses the scalar approximation.
+		'''
+		return self.electric_field.is_scalar_field
 
 	@property
 	def intensity(self):
+		'''The total intensity of the wavefront as function of 2D position on the plane.
+		'''
 		return np.abs(self.electric_field)**2
 
 	@property
 	def amplitude(self):
+		'''The amplitude of the wavefront as function of 2D position on the plane.
+		'''
 		return np.abs(self.electric_field)
 
 	@property
 	def phase(self):
+		'''The phase of the wavefront as function of 2D position on the plane.
+		'''
 		phase = np.angle(self.electric_field)
 		return Field(phase, self.electric_field.grid)
 	
 	@property
 	def real(self):
+		'''The real part of the wavefront as function of 2D position on the plane.
+		'''
 		return np.real(self.electric_field)
 	
 	@property
 	def imag(self):
+		'''The imaginary part of the wavefront as function of 2D position on the plane.
+		'''
 		return np.imag(self.electric_field)
 	
 	@property
 	def power(self):
+		'''The power of each pixel in the wavefront.
+		'''
 		return self.intensity * self.grid.weights
 	
 	@property
 	def total_power(self):
+		'''The total power in this wavefront.
+		'''
 		return np.sum(self.power)
 	
 	@total_power.setter

e25604019d41899f43509291707cc7d2a116bfca
Emiel Por <por@strw.leidenuniv.nl>
2019-11-13 15:25:01 +0100

    Formatting and adding power conservation for polarization elements.

--- tests/test_optics.py
+++ tests/test_optics.py
@@ -195,212 +195,185 @@ def test_degree_and_angle_of_polarization():
 	assert np.allclose(wf.degree_of_linear_polarization, 1 / np.sqrt(2))
 	assert np.allclose(wf.degree_of_circular_polarization, 1 / np.sqrt(2))
 
-def test_polarization_elements():
-	'''Tests to check if polarization elements have correct behaviour. 
+def mueller_matrix_for_general_linear_retarder(theta, delta):
+	'''Analytic expression Mueller matrix linear retarder. 
 
-	Using analytical Mueller matrices and set input Stokes vectors we check if 
-	the polarization elements generate the expected output Stokes vectors.
-
+	Parameters
+	----------
+	theta : scaler
+		rotation angle optic in radians 
+	delta : scalar 
+		retardance optic in radians 
 	'''
+	retarder = np.zeros((4, 4))
 
-	def general_linear_retarder(theta, delta):
-		''' Analytic expression Mueller matrix linear retarder. 
+	retarder[0, 0] = 1
 
-		Parameters
-		----------
-		theta : scaler
-		    rotation angle optic in radians 
-		delta : scalar 
-		    retardance optic in radians 
-		'''
+	retarder[1, 1] = np.cos(2 * theta)**2 + np.sin(2 * theta)**2 * np.cos(delta)
+	retarder[1, 2] = np.cos(2 * theta) * np.sin(2*theta) * (1 - np.cos(delta))
+	retarder[1, 3] = np.sin(2 * theta) * np.sin(delta)
 
-		retarder = np.zeros((4,4))
+	retarder[2, 1] = np.cos(2 * theta) * np.sin(2 * theta) * (1 - np.cos(delta))
+	retarder[2, 2] = np.cos(2 * theta)**2 * np.cos(delta) + np.sin(2 * theta)**2
+	retarder[2, 3] = -np.cos(2 * theta) * np.sin(delta)
 
-		retarder[0,0] = 1
+	retarder[3, 1] = -np.sin(2 * theta) * np.sin(delta)
+	retarder[3, 2] = np.cos(2 * theta) * np.sin(delta)
+	retarder[3, 3] = np.cos(delta)
 
-		retarder[1,1] = np.cos(2*theta)**2 + np.sin(2*theta)**2 * np.cos(delta)
-		retarder[1,2] = np.cos(2*theta) * np.sin(2*theta) * (1 - np.cos(delta))
-		retarder[1,3] = np.sin(2*theta) * np.sin(delta)
+	return retarder
 
-		retarder[2,1] = np.cos(2*theta) * np.sin(2*theta) * (1 - np.cos(delta))
-		retarder[2,2] = np.cos(2*theta)**2 * np.cos(delta) + np.sin(2*theta)**2
-		retarder[2,3] = -np.cos(2*theta) * np.sin(delta)
+def mueller_matrix_for_general_linear_polarizer(theta):
+	'''Analytic expression Mueller matrix linear polarizer. 
 
-		retarder[3,1] = -np.sin(2*theta) * np.sin(delta)
-		retarder[3,2] = np.cos(2*theta) * np.sin(delta)
-		retarder[3,3] = np.cos(delta)
+	Parameters
+	----------
+	theta : scaler
+		rotation angle optic in radians 
+	'''
+	polarizer = np.zeros((4, 4))
 
-		return retarder
+	polarizer[0, 0] = 1
+	polarizer[0, 1] = np.cos(2 * theta)
+	polarizer[0, 2] = np.sin(2 * theta)
 
-	def general_linear_polarizer(theta):
-		''' Analytic expression Mueller matrix linear polarizer. 
+	polarizer[1, 0] = np.cos(2 * theta)
+	polarizer[1, 1] = np.cos(2 * theta)**2
+	polarizer[1, 2] = 0.5 * np.sin(4*theta)
 
-		Parameters
-		----------
-		theta : scaler
-		    rotation angle optic in radians 
-		'''
-		polarizer = np.zeros((4,4))
+	polarizer[2, 0] = np.sin(2 * theta)
+	polarizer[2, 1] = 0.5 * np.sin(4 * theta)
+	polarizer[2, 2] = np.sin(2 * theta)**2
 
-		polarizer[0,0] = 1
-		polarizer[0,1] = np.cos(2*theta)
-		polarizer[0,2] = np.sin(2*theta)
+	polarizer *= 0.5
+	return polarizer
 
-		polarizer[1,0] = np.cos(2*theta)
-		polarizer[1,1] = np.cos(2*theta) ** 2
-		polarizer[1,2] = 0.5 * np.sin(4*theta)
-
-		polarizer[2,0] = np.sin(2*theta)
-		polarizer[2,1] = 0.5 * np.sin(4*theta)
-		polarizer[2,2] = np.sin(2*theta) ** 2
-
-		polarizer *= 0.5
-
-		return polarizer
-
+def test_polarization_elements():
 	N = 1
 	grid = make_pupil_grid(N)
-
 	test_field = grid.ones()
 
 	# Stokes vectors used for testing. 
 	stokes_vectors = [None,
-					np.array([1,0,0,0]), #unpolarized 
-					np.array([1,1,0,0]), # +Q polarized  
-					np.array([1,-1,0,0]), # -Q polarized  
-					np.array([1,0,1,0]), # +U polarized  
-					np.array([1,0,-1,0]), # -U polarized  
-					np.array([1,0,0,1]), # +V polarized  
-					np.array([1,0,0,-1])] # -V polarized  
+					np.array([1, 0, 0, 0]), #unpolarized 
+					np.array([1, 1, 0, 0]), # +Q polarized  
+					np.array([1, -1, 0, 0]), # -Q polarized  
+					np.array([1, 0, 1, 0]), # +U polarized  
+					np.array([1, 0, -1, 0]), # -U polarized  
+					np.array([1, 0, 0, 1]), # +V polarized  
+					np.array([1, 0, 0, -1])] # -V polarized  
 
-	# angles of the optics that will be tested
+	# Angles of the optics that will be tested.
 	angles = [-45, -22.5, 0, 22.5, 45, 90] # degrees
 
-	# testing QWPs, HWPS and polarizers
+	# Test QWPs, HWPs, polarizers and PBSs
 	for stokes_vector in stokes_vectors:
-
-		# generating the test wavefront
 		test_wf = Wavefront(test_field, stokes_vector=stokes_vector)
 
-		# set the stokes vector to unpolarized such that it can be used for tests
 		if stokes_vector is None:
-			stokes_vector = np.array([1,0,0,0]) # unpolarized
-
-		# testing the different optic angles  
+			# Set Stokes vector for futher calculation to unpolarized light.
+			stokes_vector = np.array([1, 0, 0, 0])
+ 
 		for angle in angles:
-			#--------------------------------------------
-			# testing QWP
-
-			# QWP by hcipy 
+			# Create quarterwave plate.
 			QWP_hcipy = QuarterWavePlate(np.radians(angle))
 
-			# reference QWP
-			QWP_ref = general_linear_retarder(np.radians(angle), -np.pi/2)
+			# Test if Mueller matrix is the same as reference.
+			QWP_ref = mueller_matrix_for_general_linear_retarder(np.radians(angle), -np.pi / 2)
+			assert np.allclose(QWP_hcipy.get_instance(grid, None, 1).get_instance(grid, None, 1).mueller_matrix, QWP_ref)
 
-			# testing if the Mueller matrices are the same 
-			assert np.allclose(QWP_hcipy.get_instance(grid,None,1).get_instance(grid,None,1).mueller_matrix, QWP_ref)
-
-			# propagating the test stokes vector through the hcipy and reference optics 
+			# Propagate wavefront through optical element.
 			wf_forward_QWP = QWP_hcipy.forward(test_wf)
+
+			# Test if result is the same as reference.
 			reference_stokes_post_QWP = field_dot(QWP_ref, stokes_vector)
+			assert np.allclose(wf_forward_QWP.stokes_vector[:, 0], reference_stokes_post_QWP)
 
-			# testing if the results are the same
-			assert np.allclose(wf_forward_QWP.stokes_vector[:,0], reference_stokes_post_QWP)
-
-			# testing if forward and backward propagation results in the initial state. 
+			# Test backward propagation.
 			wf_forward_backward_QWP = QWP_hcipy.backward(wf_forward_QWP)
+			assert np.allclose(wf_forward_backward_QWP.stokes_vector[:, 0], stokes_vector)
 
-			# testing if the results are the same
-			assert np.allclose(wf_forward_backward_QWP.stokes_vector[:,0], stokes_vector)
+			# Test power conservation.
+			assert np.allclose(wf_forward_QWP.I, test_wf.I)
 
-			#--------------------------------------------
-			# testing HWP
-
-			# HWP by hcipy 
+			# Create halfwave plate
 			HWP_hcipy = HalfWavePlate(np.radians(angle))
 
-			# reference QWP
-			HWP_ref = general_linear_retarder(np.radians(angle), -np.pi)
+			# Test if Mueller matrix is the same as reference.
+			HWP_ref = mueller_matrix_for_general_linear_retarder(np.radians(angle), -np.pi)
+			assert np.allclose(HWP_hcipy.get_instance(grid, None, 1).get_instance(grid, None, 1).mueller_matrix, HWP_ref)
 
-			# testing if the Mueller matrices are the same 
-			assert np.allclose(HWP_hcipy.get_instance(grid,None,1).get_instance(grid,None,1).mueller_matrix, HWP_ref)
-
-			# propagating the test stokes vector through the hcipy and reference optics 
+			# Propagate wavefront through optical element.
 			wf_forward_HWP = HWP_hcipy.forward(test_wf)
+
+			# Test if result is the same as reference.
 			reference_stokes_post_HWP = field_dot(HWP_ref, stokes_vector)
+			assert np.allclose(wf_forward_HWP.stokes_vector[:, 0], reference_stokes_post_HWP)
 
-			# testing if the results are the same
-			assert np.allclose(wf_forward_HWP.stokes_vector[:,0], reference_stokes_post_HWP)
-
-			# testing if forward and backward propagation results in the initial state. 
+			# Test backward propagation.
 			wf_forward_backward_HWP = HWP_hcipy.backward(wf_forward_HWP)
-
-			# testing if the results are the same
 			assert np.allclose(wf_forward_backward_HWP.stokes_vector[:,0], stokes_vector)
 
-			#--------------------------------------------
-			# testing polarizer 
+			# Test power conservation.
+			assert np.allclose(wf_forward_HWP.I, test_wf.I)
 
-			# Linear polarizer by hcipy 
+			# Create polarizer.
 			polarizer_hcipy = LinearPolarizer(np.radians(angle))
 
-			# reference polarizer
-			polarizer_ref = general_linear_polarizer(np.radians(angle))
+			# Test if Mueller matrix is the same as reference.
+			polarizer_ref = mueller_matrix_for_general_linear_polarizer(np.radians(angle))
+			assert np.allclose(polarizer_hcipy.get_instance(grid, None, 1).get_instance(grid, None, 1).mueller_matrix, polarizer_ref)     
 
-			# testing if the Mueller matrices are the same 
-			assert np.allclose(polarizer_hcipy.get_instance(grid,None,1).get_instance(grid,None,1).mueller_matrix, polarizer_ref)     
-
-			# propagating the test stokes vector through the hcipy and reference optics 
+			# Propagate wavefront through optical element.
 			wf_forward_polarizer = polarizer_hcipy.forward(test_wf)
+
+			# Test if result is the same as reference.
 			reference_stokes_post_polarizer = field_dot(polarizer_ref, stokes_vector)
+			assert np.allclose(wf_forward_polarizer.stokes_vector[:, 0], reference_stokes_post_polarizer)
 
-			# testing if the results are the same
-			assert np.allclose(wf_forward_polarizer.stokes_vector[:,0], reference_stokes_post_polarizer)
-
-			# propagating the wavefront backward through the polarizer
+			# Test backward propagation.
 			wf_backward_polarizer = polarizer_hcipy.backward(test_wf)
+			assert np.allclose(wf_backward_polarizer.stokes_vector[:, 0], reference_stokes_post_polarizer)
 
-			# testing if the results are the same
-			assert np.allclose(wf_backward_polarizer.stokes_vector[:,0], reference_stokes_post_polarizer)
+			# Create polarizing beam splitter
+			PBS_hcipy = PolarizingBeamSplitter(np.radians(angle))
 
-			#--------------------------------------------
-			# testing polarizing beamsplitter 
-
-			# HWP by hcipy 
-			PBS_hcipy = PolarizingBeamSplitter(np.radians(angle)) 
-
-			# reference Polarizers 
-			polarizer_1_ref = general_linear_polarizer(np.radians(angle))
-			polarizer_2_ref = general_linear_polarizer(np.radians(angle + 90))
-
-			# testing if the Mueller matrices are the same 
+			# Test if Mueller matrices are the same as reference.
+			polarizer_1_ref = mueller_matrix_for_general_linear_polarizer(np.radians(angle))
+			polarizer_2_ref = mueller_matrix_for_general_linear_polarizer(np.radians(angle + 90))
 			assert np.allclose(PBS_hcipy.mueller_matrix[0], polarizer_1_ref)     
 			assert np.allclose(PBS_hcipy.mueller_matrix[1], polarizer_2_ref)
 
-			# propagating the test stokes vector through the hcipy and reference optics 
+			# Propagate wavefront through optical element.
 			wf_forward_polarizer_1, wf_forward_polarizer_2 = PBS_hcipy.forward(test_wf)
+
+			# Test if result is the same as reference.
 			reference_stokes_post_polarizer_1 = field_dot(polarizer_1_ref, stokes_vector)
 			reference_stokes_post_polarizer_2 = field_dot(polarizer_2_ref, stokes_vector)
+			assert np.allclose(wf_forward_polarizer_1.stokes_vector[:, 0], reference_stokes_post_polarizer_1)
+			assert np.allclose(wf_forward_polarizer_2.stokes_vector[:, 0], reference_stokes_post_polarizer_2)
 
-			# testing if the results are the same
-			assert np.allclose(wf_forward_polarizer_1.stokes_vector[:,0], reference_stokes_post_polarizer_1)
-			assert np.allclose(wf_forward_polarizer_2.stokes_vector[:,0], reference_stokes_post_polarizer_2)
+			# Test power conservation.
+			assert np.allclose(test_wf.I, wf_forward_polarizer_1.I + wf_forward_polarizer_2.I)
 
-			# testing if power is conserved
-			assert np.allclose(test_wf.stokes_vector[0], wf_forward_polarizer_1.stokes_vector[0] + wf_forward_polarizer_2.stokes_vector[0])
+			# Create a rotated halfwave plate.
+			HWP = HalfWavePlate(np.radians(0))
+			rotated_HWP = RotatedJonesMatrixOpticalElement(HWP.get_instance(grid, None, 1).get_instance(grid, None, 1).jones_matrix, np.radians(angle))
 
-	# testing rotated Jones matrix function 
+			# Test if Mueller matrix is the same as reference.
+			rotated_HWP_ref = mueller_matrix_for_general_linear_retarder(np.radians(angle), -np.pi)
+			assert np.allclose(rotated_HWP.get_instance(grid, None, 1).get_instance(grid, None, 1).get_instance(grid, None, 1).mueller_matrix, rotated_HWP_ref)
 
-	# HWP by hcipy 
-	HWP_hcipy = HalfWavePlate(0)
+			# Test power conservation.
+			wf_forward_rotated_HWP = rotated_HWP.forward(test_wf)
 
-	for angle in angles:
+			# Test if result is the same as reference.
+			reference_stokes_post_rotated_HWP = field_dot(rotated_HWP_ref, stokes_vector)
+			assert np.allclose(wf_forward_rotated_HWP.stokes_vector[:, 0], reference_stokes_post_rotated_HWP)
 
-		# rotating the HWP 
-		rotated_HWP_hcipy = RotatedJonesMatrixOpticalElement(HWP_hcipy.get_instance(grid,None,1).get_instance(grid,None,1).jones_matrix, np.radians(angle))
+			# Test backward propagation.
+			wf_forward_backward_rotated_HWP = rotated_HWP.backward(wf_forward_rotated_HWP)
+			assert np.allclose(wf_forward_backward_rotated_HWP.stokes_vector[:,0], stokes_vector)
 
-		# calculating a reference HWP at the same angle
-		HWP_ref = general_linear_retarder(np.radians(angle), -np.pi)
-
-		# testing if the Mueller matrices are the same 
-		assert np.allclose(rotated_HWP_hcipy.get_instance(grid,None,1).get_instance(grid,None,1).get_instance(grid,None,1).mueller_matrix, HWP_ref)
+			# Test power conservation.
+			assert np.allclose(test_wf.I, wf_forward_rotated_HWP.I)

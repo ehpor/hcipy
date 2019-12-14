beb56221b355c0a5a9f8228d1df2aa6568ef6a34
Emiel Por <por@strw.leidenuniv.nl>
2019-12-14 14:29:45 +0100

    Add test for vector vortex coronagraph

--- tests/test_coronagraphy.py
+++ tests/test_coronagraphy.py
@@ -12,8 +12,34 @@ def test_vortex_coronagraph():
 	lyot = circular_aperture(0.99)
 	lyot = evaluate_supersampled(lyot, pupil_grid, 8) > 1 - 1e-5
 
-	for charge in [2,4,6,8]:
+	for charge in [2, 4, 6, 8]:
 		vortex = VortexCoronagraph(pupil_grid, charge, levels=6)
+
+		wf = Wavefront(aperture)
+		wf.total_power = 1
+
+		img_ref = prop(wf)
+
+		wf = vortex(wf)
+		wf.electric_field *= lyot
+		img = prop(wf)
+
+		assert img.total_power < 1e-6
+		assert img.intensity.max() / img_ref.intensity.max() < 1e-8
+
+def test_vector_vortex_coronagraph():
+	pupil_grid = make_pupil_grid(256)
+	focal_grid = make_focal_grid(4, 32)
+	prop = FraunhoferPropagator(pupil_grid, focal_grid)
+
+	aperture = circular_aperture(1)
+	aperture = evaluate_supersampled(aperture, pupil_grid, 8)
+
+	lyot = circular_aperture(0.99)
+	lyot = evaluate_supersampled(lyot, pupil_grid, 8) > 1 - 1e-5
+
+	for charge in [2, 4, 6, 8]:
+		vortex = VectorVortexCoronagraph(charge, levels=6)
 
 		wf = Wavefront(aperture)
 		wf.total_power = 1
@@ -38,7 +64,7 @@ def test_ravc():
 
 		aper = aperture > 1e-5
 
-		for charge in [2,4]:
+		for charge in [2, 4]:
 			pupil, lyot = make_ravc_masks(co, charge, lyot_undersize=0.02)
 
 			pupil = evaluate_supersampled(pupil, pupil_grid, 4)

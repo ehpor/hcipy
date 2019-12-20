38b1ef548da511bb9523b662a240eefcd6cb29f8
Sebastiaan Haffert <shaffert@email.arizona.edu>
2019-12-20 12:56:23 -0700

    Add the different micro-lenses

--- hcipy/optics/micro_lens_array.py
+++ hcipy/optics/micro_lens_array.py
@@ -1,6 +1,7 @@
 import numpy as np
 from .apodization import SurfaceApodizer
 from .optical_element import OpticalElement
+from .surface_profiles import *
 from ..field import Field
 
 def closest_points(point_grid, evaluated_grid):
@@ -12,6 +13,19 @@ def closest_points(point_grid, evaluated_grid):
 	return Field(i, evaluated_grid), Field(d, evaluated_grid)
 
 class MicroLensArray(OpticalElement):
+	'''A parabolic micro-lens array.
+
+	Parameters
+	----------
+	input_grid : Grid
+		The grid on which the micro-lens array is evaluated.
+	lenslet_grid : Grid
+		A grid containing the lenslet positions.
+	focal_length : scalar
+		The focal length of the micro-lenses
+	lenslet_shape : field generator
+		The shape of a lenslet.
+	'''
 	def __init__(self, input_grid, lenslet_grid, focal_length, lenslet_shape=None):
 		self.input_grid = input_grid
 		self.focal_length = focal_length
@@ -33,61 +47,112 @@ class MicroLensArray(OpticalElement):
 				
 				self.mla_opd[mask] = (-1 / (2*focal_length)) * (shifted_grid.x[mask]**2 + shifted_grid.y[mask]**2)
 				self.mla_index[mask] = i
-			
+		
+		self.mla_surface = SurfaceApodizer(self.mla_opd, 2)
+
 	def forward(self, wavefront):
-		wf = wavefront.copy()
-		wf.electric_field *= np.exp(1j * self.mla_opd * 2*np.pi / wf.wavelength)
-		return wf
+		return self.mla_surface.forward(wavefront)
 		
 	def backward(self, wavefront):
-		wf = wavefront.copy()
-		wf.electric_field /= np.exp(1j * self.mla_opd * 2*np.pi / wf.wavelength)
-		return wf
+		return self.mla_surface.backward(wavefront)
 
 
-class EvenAsphereMicroLensArray(OpticalElement):
-	def __init__(self, input_grid, lenslet_grid, radius_of_curvature, wavelength=1, refractive_index=1.5, conic_constant=0, aspheric_coefficients=[], lenslet_shape=None):
-		self.input_grid = input_grid
+class SphericalMicroLensArray(OpticalElement):
+	'''An even asphere micro-lens array.
+
+	Parameters
+	----------
+	input_grid : Grid
+		The grid on which the micro-lens array is evaluated.
+	lenslet_grid : Grid
+		A grid containing the lenslet positions.
+	radius_of_curvature : scalar
+		The radius of curvature of the micro-lenses.
+	lenslet_shape : field generator
+		The shape of a lenslet.
+	refractive_index : scalar or function
+		The refractive index of the material.
+	conic_constant : scalar
+		The conic constant of the micro-lenses.
+	aspheric_coefficients : array_like
+		The aspheric coefficients of the micro-lenses.
+	'''
+	def __init__(self, input_grid, lenslet_grid, radius_of_curvature, lenslet_shape, refractive_index=1.5):
 		
+		self.input_grid = input_grid		
 		self.mla_grid = lenslet_grid
 		self.n = refractive_index
 		self.radius_of_curvature = radius_of_curvature
-		self.conic_constant = conic_constant
 
-		#self.focal_length = radius_of_curvature/(refractive_index-1)
-
-		self.mla_index = Field(-np.ones(self.input_grid.size), self.input_grid)
-		self.mla_opd = Field(np.zeros(self.input_grid.size), self.input_grid)
-		self.mla_amp = Field(np.zeros(self.input_grid.size), self.input_grid)
-		self.mla_cin = Field(np.zeros(self.input_grid.size), self.input_grid)
+		self.mla_index = -self.input_grid.ones()
+		self.mla_opd = self.input_grid.zeros()
+		self.surface_sag = spherical_surface_sag(radius_of_curvature)
 
 		for i, (x,y) in enumerate(lenslet_grid.as_('cartesian').points):
 			shifted_grid = input_grid.shifted((x,y))
 			mask = lenslet_shape(shifted_grid) != 0
-			
+			subset_grid = shifted_grid.subset(mask)
+
+			# Check if the micro-lens is within the field of view.
 			if np.count_nonzero(mask) > 0:
-				xs = shifted_grid.x[mask]
-				ys = shifted_grid.y[mask]
-				r = np.hypot(xs, ys)
-
-				# Start with a conic surface
-				curvature = 1/self.radius_of_curvature
-				alpha = 1 - (1+self.conic_constant) * curvature**2 * r**2
-				self.mla_opd[mask] = curvature * r**2 /(1+np.sqrt(alpha))
-
-				# Add aspheric coefficients
-				# Only use the even modes and start at 4, because 0 is piston and 2 is the conic surface
-				for ai, coef in enumerate(aspheric_coefficients):
-					power_index = 4 + ai * 2
-					self.mla_opd[mask] += -coef * (r/r.max())**power_index
-
 				self.mla_index[mask] = i
-		self.mla_opd = -self.mla_opd
-				
-		self.surface = SurfaceApodizer(self.mla_opd, refractive_index)
+				self.mla_opd[mask] += self.surface_sag(subset_grid)
+			
+		self.mla_surface = SurfaceApodizer(self.mla_opd, refractive_index)
 
 	def forward(self, wavefront):
-		return self.surface.forward(wavefront)
+		return self.mla_surface.forward(wavefront)
 		
 	def backward(self, wavefront):
-		return self.surface.backward(wavefront)
+		return self.mla_surface.backward(wavefront)
+
+class EvenAsphereMicroLensArray(OpticalElement):
+	'''An even asphere micro-lens array.
+
+	Parameters
+	----------
+	input_grid : Grid
+		The grid on which the micro-lens array is evaluated.
+	lenslet_grid : Grid
+		A grid containing the lenslet positions.
+	radius_of_curvature : scalar
+		The radius of curvature of the micro-lenses.
+	lenslet_shape : field generator
+		The shape of a lenslet.
+	refractive_index : scalar or function
+		The refractive index of the material.
+	conic_constant : scalar
+		The conic constant of the micro-lenses.
+	aspheric_coefficients : array_like
+		The aspheric coefficients of the micro-lenses.
+	'''
+	def __init__(self, input_grid, lenslet_grid, radius_of_curvature, lenslet_shape, refractive_index=1.5, conic_constant=0, aspheric_coefficients=[]):
+		
+		self.input_grid = input_grid		
+		self.mla_grid = lenslet_grid
+		self.n = refractive_index
+		self.radius_of_curvature = radius_of_curvature
+		self.conic_constant = conic_constant
+		self.aspheric_coefficients = aspheric_coefficients
+
+		self.mla_index = -self.input_grid.ones()
+		self.mla_opd = self.input_grid.zeros()
+		self.surface_sag = even_aspheric_surface_sag(radius_of_curvature, conic_constant, aspheric_coefficients)
+
+		for i, (x,y) in enumerate(lenslet_grid.as_('cartesian').points):
+			shifted_grid = input_grid.shifted((x,y))
+			mask = lenslet_shape(shifted_grid) != 0
+			subset_grid = shifted_grid.subset(mask)
+
+			# Check if the micro-lens is within the field of view.
+			if np.count_nonzero(mask) > 0:
+				self.mla_index[mask] = i
+				self.mla_opd[mask] += self.surface_sag(subset_grid)
+			
+		self.mla_surface = SurfaceApodizer(self.mla_opd, refractive_index)
+
+	def forward(self, wavefront):
+		return self.mla_surface.forward(wavefront)
+		
+	def backward(self, wavefront):
+		return self.mla_surface.backward(wavefront)

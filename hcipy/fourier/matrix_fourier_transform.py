8a82a8de215b5eacc2ea9e03aa6f41985b516f61
Emiel Por <por@strw.leidenuniv.nl>
2020-05-08 13:41:36 +0200

    Efficiently apply weights depending on dimensions.

--- hcipy/fourier/matrix_fourier_transform.py
+++ hcipy/fourier/matrix_fourier_transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import blas
 from .fourier_transform import FourierTransform, multiplex_for_tensor_fields
 from ..field import Field
 
@@ -17,11 +18,13 @@ class MatrixFourierTransform(FourierTransform):
 		self.input_grid = input_grid
 		self.output_grid = output_grid
 
-		self.shape = input_grid.shape
+		self.shape_input = input_grid.shape
+		self.shape_output = output_grid.shape
+
 		self.ndim = input_grid.ndim
 
 		self.weights_input = input_grid.weights.ravel()
-		if np.allclose(self.weights_input, self.weights_input[0]):
+		if np.all(self.weights_input == self.weights_input[0]):
 			self.weights_input = self.weights_input[0]
 
 		self.weights_output = output_grid.weights.ravel()
@@ -29,10 +32,10 @@ class MatrixFourierTransform(FourierTransform):
 			self.weights_output = self.weights_output[0]
 
 		if self.ndim == 1:
-			self.M = np.exp(-1j * np.dot(output_grid.x[:,np.newaxis], input_grid.x[np.newaxis,:]))
+			self.M = np.exp(-1j * np.outer(output_grid.x, input_grid.x))
 		elif self.ndim == 2:
-			self.M1 = np.exp(-1j * np.dot(output_grid.coords.separated_coords[1][:,np.newaxis], input_grid.coords.separated_coords[1][np.newaxis,:]))
-			self.M2 = np.exp(-1j * np.dot(output_grid.coords.separated_coords[0][:,np.newaxis], input_grid.coords.separated_coords[0][np.newaxis,:])).T
+			self.M1 = np.exp(-1j * np.outer(output_grid.coords.separated_coords[1], input_grid.separated_coords[1]))
+			self.M2 = np.exp(-1j * np.outer(input_grid.coords.separated_coords[0], output_grid.separated_coords[0]))
 
 	@multiplex_for_tensor_fields
 	def forward(self, field):
@@ -40,8 +43,17 @@ class MatrixFourierTransform(FourierTransform):
 			f = field * self.weights_input
 			res = np.dot(self.M, f)
 		elif self.ndim == 2:
-			f = (field * self.weights_input).reshape(self.shape)
-			res = np.dot(np.dot(self.M1, f), self.M2).reshape(-1)
+			if np.isscalar(self.weights_input):
+				f = field.reshape(self.shape_input)
+				if np.dtype('complex128') == field.dtype:
+					res = blas.zgemm(self.weights_input, blas.zgemm(1, self.M2.T, f.T), self.M1.T)
+				elif self.input_grid.size > self.output_grid.size:
+					res = np.dot(np.dot(self.M1, f), self.M2).reshape(-1) * self.weights_input
+				else:
+					res = np.dot(np.dot(self.M1, f * self.weights_input), self.M2).reshape(-1)
+			else:
+				f = (field * self.weights_input).reshape(self.shape_input)
+				res = np.dot(np.dot(self.M1, f), self.M2).reshape(-1)
 
 		return Field(res, self.output_grid)
 
@@ -49,10 +61,23 @@ class MatrixFourierTransform(FourierTransform):
 	def backward(self, field):
 		if self.ndim == 1:
 			f = field * self.weights_output
-			res = np.dot(self.M.conj().T, f)
+			res = np.dot(self.M.conj().T, f) / (2 * np.pi)
 		elif self.ndim == 2:
-			f = (field * self.weights_output).reshape(self.output_grid.shape)
-			res = np.dot(np.dot(self.M1.conj().T, f), self.M2.conj().T).reshape(-1)
+			if np.dtype('complex128') == field.dtype:
+				if np.isscalar(self.weights_output):
+					f = field.reshape(self.shape_output)
+					w = self.weights_output / (2 * np.pi)**2
+					res = blas.zgemm(1, blas.zgemm(w, self.M2.T, f.T, trans_a=2), self.M1.T, trans_b=2).T.reshape(-1)
+				else:
+					f = (field * self.weights_output).reshape(self.shape_output)
+					w = 1 / (2 * np.pi)**2
+					res = blas.zgemm(1, blas.zgemm(w, self.M2.T, f.T, trans_a=2), self.M1.T, trans_b=2).T.reshape(-1)
+			else:
+				if np.isscalar(self.weights_output) and self.input_grid.size < self.output_grid.size:
+					f = field.reshape(self.shape_output)
+					res = np.dot(np.dot(self.M1.conj().T, f), self.M2.conj().T).reshape(-1) * self.weights_output
+				else:
+					f = (field * self.weights_output).reshape(self.shape_output)
+					res = np.dot(np.dot(self.M1.conj().T, f), self.M2.conj().T).reshape(-1)
 
-		res /= (2 * np.pi)**self.ndim
 		return Field(res, self.input_grid)

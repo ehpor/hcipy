e2b6cf510ccf47c3036452671eb3751a0828b5da
Emiel Por <por@strw.leidenuniv.nl>
2020-05-30 16:39:56 +0200

    Add TensorFlow as a backend for MFTs

--- hcipy/fourier/matrix_fourier_transform.py
+++ hcipy/fourier/matrix_fourier_transform.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.linalg import blas
 from .fourier_transform import FourierTransform, multiplex_for_tensor_fields
-from ..field import Field
+from ..field import Field, NumpyField, TensorFlowField
 from ..config import Configuration
 import numexpr as ne
 
@@ -71,6 +71,10 @@ class MatrixFourierTransform(FourierTransform):
 		self.matrices_dtype = None
 		self.intermediate_dtype = None
 		self._remove_matrices()
+
+		self._tf_M = None
+		self._tf_M1 = None
+		self._tf_M2 = None
 
 	def _compute_matrices(self, dtype):
 		'''Compute the matrices for the MFT using the specified data type.
@@ -157,36 +161,54 @@ class MatrixFourierTransform(FourierTransform):
 			The Fourier transform of the field.
 		'''
 		self._compute_matrices(field.dtype)
-		field = field.astype(self.matrices_dtype, copy=False)
 
-		if self.ndim == 1:
-			f = field * self.weights_input
-			res = np.dot(self.M, f)
-		elif self.ndim == 2:
-			# Use handcoded BLAS call. BLAS is better when all inputs are Fortran ordered,
-			# so we apply matrix multiplications on the transpose of each of the arrays
-			# (which are C ordered).
-			if field.dtype == 'complex64':
-				gemm = blas.cgemm
-			else:
-				gemm = blas.zgemm
+		if field.backend == 'numpy':
+			field = field.astype(self.matrices_dtype, copy=False)
 
-			if np.isscalar(self.weights_input):
-				# Weights can be included in the gemm call as that multiplication
-				# happens anyway (and it saves an array copy).
-				f = field.reshape(self.shape_input)
-				alpha = self.weights_input
-			else:
-				# Fallback in case the weights is not a scalar.
-				f = (field * self.weights_input).reshape(self.shape_input)
-				alpha = 1
+			if self.ndim == 1:
+				f = field * self.weights_input
+				res = np.dot(self.M, f)
+			elif self.ndim == 2:
+				# Use handcoded BLAS call. BLAS is better when all inputs are Fortran ordered,
+				# so we apply matrix multiplications on the transpose of each of the arrays
+				# (which are C ordered).
+				if field.dtype == 'complex64':
+					gemm = blas.cgemm
+				else:
+					gemm = blas.zgemm
 
-			gemm(1, self.M2.T, f.T, c=self.intermediate_array.T, overwrite_c=True)
-			res = gemm(alpha, self.intermediate_array.T, self.M1.T).T.reshape(-1)
+				if np.isscalar(self.weights_input):
+					# Weights can be included in the gemm call as that multiplication
+					# happens anyway (and it saves an array copy).
+					f = field.reshape(self.shape_input)
+					alpha = self.weights_input
+				else:
+					# Fallback in case the weights is not a scalar.
+					f = (field * self.weights_input).reshape(self.shape_input)
+					alpha = 1
+
+				gemm(1, self.M2.T, f.T, c=self.intermediate_array.T, overwrite_c=True)
+				res = gemm(alpha, self.intermediate_array.T, self.M1.T).T.reshape(-1)
+
+			res = NumpyField(res, self.output_grid)
+		elif field.backend == 'tensorflow':
+			import tensorflow as tf
+
+			f = tf.cast(tf.reshape(field.arr, self.shape_input), self.matrices_dtype)
+			f *= self.weights_input
+
+			if self.ndim == 1:
+				res = tf.matmul(self.tf_M, f)
+			elif self.ndim == 2:
+				res = tf.matmul(self.M1, tf.matmul(f, self.M2))
+
+			res = TensorFlowField(tf.reshape(res, (-1,)), self.output_grid)
+		else:
+			raise ValueError('Fields with this backend are not supported.')
 
 		self._remove_matrices()
 
-		return Field(res, self.output_grid)
+		return res
 
 	@multiplex_for_tensor_fields
 	def backward(self, field):
@@ -203,35 +225,77 @@ class MatrixFourierTransform(FourierTransform):
 			The inverse Fourier transform of the field.
 		'''
 		self._compute_matrices(field.dtype)
-		field = field.astype(self.matrices_dtype, copy=False)
 
-		if self.ndim == 1:
-			f = field * self.weights_output
-			res = np.dot(self.M.conj().T, f)
-		elif self.ndim == 2:
-			# Use handcoded BLAS call. BLAS is better when all inputs are Fortran ordered,
-			# so we apply matrix multiplications on the transpose of each of the arrays
-			# (which are C ordered). Adjoint is handled by GEMM, which avoids an array
-			# copy for these array as well.
-			if field.dtype == 'complex64':
-				gemm = blas.cgemm
-			else:
-				gemm = blas.zgemm
+		if field.backend == 'numpy':
+			field = field.astype(self.matrices_dtype, copy=False)
 
-			if np.isscalar(self.weights_output):
-				# Weights can be included in the gemm call as that multiplication
-				# happens anyway (and it saves an array copy).
-				f = field.reshape(self.shape_output)
-				alpha = self.weights_output
-			else:
-				# Fallback in case the weights is not a scalar.
-				f = (field * self.weights_output).reshape(self.shape_output)
-				alpha = 1
+			if self.ndim == 1:
+				f = field * self.weights_output
+				res = np.dot(self.M.conj().T, f)
+			elif self.ndim == 2:
+				# Use handcoded BLAS call. BLAS is better when all inputs are Fortran ordered,
+				# so we apply matrix multiplications on the transpose of each of the arrays
+				# (which are C ordered). Adjoint is handled by GEMM, which avoids an array
+				# copy for these array as well.
+				if field.dtype == 'complex64':
+					gemm = blas.cgemm
+				else:
+					gemm = blas.zgemm
 
-			# Use trans_a=2 and trans_b=2 to apply the conjugte transpose on the a and b arrays.
-			gemm(1, f.T, self.M1.T, trans_b=2, c=self.intermediate_array.T, overwrite_c=True)
-			res = gemm(alpha, self.M2.T, self.intermediate_array.T, trans_a=2).T.reshape(-1)
+				if np.isscalar(self.weights_output):
+					# Weights can be included in the gemm call as that multiplication
+					# happens anyway (and it saves an array copy).
+					f = field.reshape(self.shape_output)
+					alpha = self.weights_output
+				else:
+					# Fallback in case the weights is not a scalar.
+					f = (field * self.weights_output).reshape(self.shape_output)
+					alpha = 1
+
+				# Use trans_a=2 and trans_b=2 to apply the conjugte transpose on the a and b arrays.
+				gemm(1, f.T, self.M1.T, trans_b=2, c=self.intermediate_array.T, overwrite_c=True)
+				res = gemm(alpha, self.M2.T, self.intermediate_array.T, trans_a=2).T.reshape(-1)
+
+			res = NumpyField(res, self.input_grid)
+		elif field.backend == 'tensorflow':
+			import tensorflow as tf
+
+			f = tf.cast(tf.reshape(field.arr, self.shape_output), self.matrices_dtype)
+			f *= self.weights_output
+
+			if self.ndim == 1:
+				res = tf.matmul(self.tf_M, f, adjoint_a=True)
+			elif self.ndim == 2:
+				res = tf.matmul(self.tf_M1, tf.matmul(f, self.tf_M2, adjoint_b=True), adjoint_a=True)
+
+			res = TensorFlowField(tf.reshape(res, (-1,)), self.input_grid)
+		else:
+			raise ValueError('Fields with this backend are not supported.')
 
 		self._remove_matrices()
 
-		return Field(res, self.input_grid)
+		return res
+
+	@property
+	def tf_M(self):
+		import tensorflow as tf
+
+		if self._tf_M is None:
+			self._tf_M = tf.convert_to_tensor(self.M)
+		return self._tf_M
+
+	@property
+	def tf_M1(self):
+		import tensorflow as tf
+
+		if self._tf_M1 is None:
+			self._tf_M1 = tf.convert_to_tensor(self.M1)
+		return self._tf_M1
+
+	@property
+	def tf_M2(self):
+		import tensorflow as tf
+
+		if self._tf_M2 is None:
+			self._tf_M2 = tf.convert_to_tensor(self.M2)
+		return self._tf_M2

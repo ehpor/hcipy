d5a0e179d0a91a470160e58fdc58d3f546db814f
Sebastiaan Haffert <sebastiaanhaffert@gmail.com>
2021-02-17 09:40:38 -0700

    added documentation to the thin lens

--- hcipy/optics/optics.py
+++ hcipy/optics/optics.py
@@ -3,6 +3,23 @@ from .surface_profiles import parabolic_surface_sag
 from .apodization import SurfaceApodizer
 
 def make_thin_lens(focal_length, refractive_index, reference_wavelength):
+	'''Create a parabolic thin lens.
+
+	Parameters
+	----------
+	focal_length : scalar
+		The focal length of the thin lens.
+	refractive_index : scalar or function of wavelength
+		The refractive index of the lens material.
+	reference_wavelength : scalar
+		The wavelength for which the focal length is defined.
+	
+	Returns
+	-------
+	SurfaceApodizer
+		The thin lens optical element.
+	'''
+
 	n0 = refractive_index(reference_wavelength)
 	radius_of_curvature = focal_length * (n0 - 1)
 	sag = parabolic_surface_sag(-radius_of_curvature)

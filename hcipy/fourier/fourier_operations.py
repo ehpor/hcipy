29ab47da5588757952deccedbd95f173f35f00cf
Emiel Por <epor@stsci.edu>
2021-03-24 18:06:25 -0400

    Make mkl_fft truly optional (bugfix).

--- hcipy/fourier/fourier_operations.py
+++ hcipy/fourier/fourier_operations.py
@@ -5,8 +5,10 @@ from ..field import Field, field_dot, field_conjugate_transpose
 
 try:
 	import mkl_fft as _fft_module
+	_use_mkl = True
 except ImportError:
 	_fft_module = np.fft
+	_use_mkl = False
 
 class FourierFilter(object):
 	'''A filter in the Fourier domain.
@@ -115,8 +117,11 @@ class FourierFilter(object):
 			f[c] = field.shaped
 
 		# Don't overwrite f if it's the input array.
-		overwrite_x = self.cutout is not None and field.grid.ndim > 1
-		f = _fft_module.fftn(f, axes=tuple(range(-self.input_grid.ndim, 0)), overwrite_x=overwrite_x)
+		if _use_mkl:
+			kwargs = {'overwrite_x': self.cutout is not None and field.grid.ndim > 1}
+		else:
+			kwargs = {}
+		f = _fft_module.fftn(f, axes=tuple(range(-self.input_grid.ndim, 0)), **kwargs)
 
 		if (self._transfer_function.ndim - self.internal_grid.ndim) == 2:
 			# The transfer function is a matrix field.
@@ -139,7 +144,11 @@ class FourierFilter(object):
 
 			f *= tf
 
-		f = _fft_module.ifftn(f, axes=tuple(range(-self.input_grid.ndim, 0)), overwrite_x=True)
+		if _use_mkl:
+			kwargs = {'overwrite_x': True}
+		else:
+			kwargs = {}
+		f = _fft_module.ifftn(f, axes=tuple(range(-self.input_grid.ndim, 0)), **kwargs)
 
 		s = f.shape[:-self.internal_grid.ndim] + (-1,)
 		if self.cutout is None:

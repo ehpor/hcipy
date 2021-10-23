085fb6f8bf2724576f85ac2562ac051d44e936fa
Emiel Por <epor@stsci.edu>
2021-10-23 01:15:38 -0400

    Update Python version support in PyPI.
    Leftover from 0.4.0 release.

--- setup.py
+++ setup.py
@@ -54,9 +54,9 @@ setup(name='hcipy',
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.5",
 		"Programming Language :: Python :: 3.6",
 		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
 		"Topic :: Scientific/Engineering :: Astronomy"],
 	license_file="LICENSE"
 )

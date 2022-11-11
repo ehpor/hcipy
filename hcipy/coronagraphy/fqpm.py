11b38f91d0ec7736bab4c3f58dff57085d72c902
Iva Laginja <iva.laginja@obspm.fr>
2022-11-11 01:03:49 +0100

    Use better defaults to multi-scale input parameters in FQPM.

--- hcipy/coronagraphy/fqpm.py
+++ hcipy/coronagraphy/fqpm.py
@@ -4,6 +4,6 @@ from .multi_scale import MultiScaleCoronagraph
 from ..field import Field
 
 class FQPMCoronagraph(MultiScaleCoronagraph):
-    def __init__(self, input_grid, lyot_stop=None, q=4, scaling_factor=4, window_size=128):
+	def __init__(self, input_grid, lyot_stop=None, q=4, scaling_factor=10, window_size=128):
 		phasor = lambda grid: Field(np.exp(1j * Field(np.abs(np.heaviside(grid.x, 0.5) - np.heaviside(grid.y, 0.5)) * np.pi, grid)), grid)
 		super().__init__(input_grid, phasor, lyot_stop, q, scaling_factor, window_size)

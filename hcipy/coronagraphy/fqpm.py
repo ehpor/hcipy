a5474c4892c2edbe0c539ce9c0023673536abfcb
Iva Laginja <iva.laginja@obspm.fr>
2023-06-27 16:02:15 +0200

    Use sign function to create complex phase mask for FQPM

--- hcipy/coronagraphy/fqpm.py
+++ hcipy/coronagraphy/fqpm.py
@@ -5,5 +5,5 @@ from ..field import Field
 
 class FQPMCoronagraph(MultiScaleCoronagraph):
 	def __init__(self, input_grid, lyot_stop=None, q=1024, scaling_factor=4, window_size=32):
-		phasor = lambda grid: Field(np.exp(1j * Field(np.abs(np.heaviside(grid.x, 0.5) - np.heaviside(grid.y, 0.5)) * np.pi, grid)), grid)
-		super().__init__(input_grid, phasor, lyot_stop, q, scaling_factor, window_size)
+		complex_mask = lambda grid: Field(np.sign(grid.x) * np.sign(grid.y), grid).astype('complex')
+		super().__init__(input_grid, complex_mask, lyot_stop, q, scaling_factor, window_size)

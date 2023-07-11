658f84fbb3006e01731fb6dc4599f14554b9dbb7
Iva Laginja <iva.laginja@obspm.fr>
2023-07-11 17:40:59 +0200

    Convert tab indentations to spaces

--- hcipy/coronagraphy/fqpm.py
+++ hcipy/coronagraphy/fqpm.py
@@ -4,6 +4,6 @@ from .multi_scale import MultiScaleCoronagraph
 from ..field import Field
 
 class FQPMCoronagraph(MultiScaleCoronagraph):
-	def __init__(self, input_grid, lyot_stop=None, q=1024, scaling_factor=4, window_size=32):
-		complex_mask = lambda grid: Field(np.sign(grid.x) * np.sign(grid.y), grid).astype('complex')
-		super().__init__(input_grid, complex_mask, lyot_stop, q, scaling_factor, window_size)
+    def __init__(self, input_grid, lyot_stop=None, q=1024, scaling_factor=4, window_size=32):
+        complex_mask = lambda grid: Field(np.sign(grid.x) * np.sign(grid.y), grid).astype('complex')
+        super().__init__(input_grid, complex_mask, lyot_stop, q, scaling_factor, window_size)

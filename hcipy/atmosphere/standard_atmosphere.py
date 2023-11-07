90fa48b68e2bd74b1d8b486d76c2e2ff04e9b3c2
Emiel Por <epor@stsci.edu>
2023-11-07 14:16:13 -0500

    Generalize function signatures for all make_*_atmospheric_layers().

--- hcipy/atmosphere/standard_atmosphere.py
+++ hcipy/atmosphere/standard_atmosphere.py
@@ -1,9 +1,10 @@
 from .atmospheric_model import Cn_squared_from_fried_parameter, MultiLayerAtmosphere
 from .infinite_atmospheric_layer import InfiniteAtmosphericLayer
+from ..dev import deprecated_name_changed
 
 import numpy as np
 
-def make_standard_atmospheric_layers(input_grid, L0=10):
+def make_mauna_kea_atmospheric_layers(input_grid, cn_squared=None, outer_scale=None):
     '''Create a standardized multi-layer atmosphere.
 
     The layer parameters are taken from [Guyon2005]_, in turn derived
@@ -19,28 +20,55 @@ def make_standard_atmospheric_layers(input_grid, L0=10):
     ----------
     input_grid : Grid
         The input grid for the atmospheric layers.
-    L0 : scalar
-        The outer scale of the atmosphere
+    cn_squared : scalar
+        The integrated Cn^2 value for all layers. If this is None (default),
+        then a Fried parameter of 0.2m at 500nm is used.
+    outer_scale : scalar or None
+        The outer scale of the atmosphere. If this is None (default), then an
+        outer scale of 10m is used.
 
     Returns
     -------
     list
         A list of turbulence layers.
     '''
+    if cn_squared is None:
+        cn_squared = Cn_squared_from_fried_parameter(0.2)
+
+    if outer_scale is None:
+        outer_scale = 10
+
     heights = np.array([500, 1000, 2000, 4000, 8000, 16000])
     velocities = np.array([10, 10, 10, 10, 10, 10])
-    Cn_squared = np.array([0.2283, 0.0883, 0.0666, 0.1458, 0.3350, 0.1350]) * 1e-12
+    cn_squared_values = np.array([0.2283, 0.0883, 0.0666, 0.1458, 0.3350, 0.1350])
+
+    cn_squared_values *= cn_squared / np.sum(cn_squared_values)
 
     layers = []
-    for h, v, cn in zip(heights, velocities, Cn_squared):
-        layers.append(InfiniteAtmosphericLayer(input_grid, cn, L0, v, h, 2))
+    for h, v, cn in zip(heights, velocities, cn_squared_values):
+        layers.append(InfiniteAtmosphericLayer(input_grid, cn, outer_scale, v, h, 2))
 
     return layers
 
-def make_mauna_kea_atmospheric_layers(input_grid, cn_squared=None, outer_scale=None):
-    return make_standard_atmospheric_layers(input_grid, outer_scale)
+@deprecated_name_changed(make_mauna_kea_atmospheric_layers)
+def make_standard_atmospheric_layers(input_grid, L0=10):
+    '''Make a standard set of atmospheric layers.
 
-def make_las_campanas_atmospheric_layers(input_grid, r0=0.16, L0=25, wavelength=550e-9):
+    Parameters
+    ----------
+    input_grid : Grid
+        The input grid for the atmospheric layers.
+    L0 : scalar
+        The outer scale of the atmosphere.
+
+    Returns
+    -------
+    list
+        A list of turbulence layers.
+    '''
+    return make_mauna_kea_atmospheric_layers(input_grid, outer_scale=L0)
+
+def make_las_campanas_atmospheric_layers(input_grid, cn_squared=None, outer_scale=None):
     '''Creates a multi-layer atmosphere for the Las Campanas Observatory site.
 
     The layer parameters are taken from [Males2019]_ who based it on site testing from [Prieto2010]_ and [Osip2011]_ .
@@ -58,34 +86,42 @@ def make_las_campanas_atmospheric_layers(input_grid, r0=0.16, L0=25, wavelength=
     ----------
     input_grid : Grid
         The input grid for the atmospheric layers.
-    r0 : scalar
-        The integrated Cn^2 value for the atmosphere.
-    L0 : scalar
-        The outer scale of the atmosphere
-    wavelength : scalar
-        The wavelength in meters at which to calculate the Fried parameter (default: 550nm).
+    cn_squared : scalar
+        The integrated Cn^2 value for all layers. If this is None (default),
+        then a Fried parameter of 0.16m at 500nm is used.
+    outer_scale : scalar or None
+        The outer scale of the atmosphere. If this is None (default), then an
+        outer scale of 25m is used.
 
     Returns
     -------
     list
         A list of turbulence layers.
     '''
+    if cn_squared is None:
+        cn_squared = Cn_squared_from_fried_parameter(0.16)
+
+    if outer_scale is None:
+        outer_scale = 25
+
     heights = np.array([250, 500, 1000, 2000, 4000, 8000, 16000])
     velocities = np.array([10, 10, 20, 20, 25, 30, 25])
 
-    integrated_cn_squared = Cn_squared_from_fried_parameter(r0, wavelength=500e-9)
-    Cn_squared = np.array([0.42, 0.03, 0.06, 0.16, 0.11, 0.10, 0.12]) * integrated_cn_squared
+    cn_squared_values = np.array([0.42, 0.03, 0.06, 0.16, 0.11, 0.10, 0.12])
+
+    cn_squared_values *= cn_squared / np.sum(cn_squared_values)
 
     layers = []
-    for h, v, cn in zip(heights, velocities, Cn_squared):
-        layers.append(InfiniteAtmosphericLayer(input_grid, cn, L0, v, h, 2))
+    for h, v, cn in zip(heights, velocities, cn_squared_values):
+        layers.append(InfiniteAtmosphericLayer(input_grid, cn, outer_scale, v, h, 2))
 
     return layers
 
-def make_keck_atmospheric_layers(input_grid):
+def make_keck_atmospheric_layers(input_grid, cn_squared=None, outer_scale=20):
     '''Creates a multi-layer atmosphere for Keck Observatory.
 
-    The atmospheric parameters are based off of [Keck AO note 303]_.
+    The atmospheric parameters are based off of [Keck AO note 303]_. The default
+    Fried parameter is 0.2m and the default outer scale is 20m.
 
     .. [Keck AO note 303] https://www2.keck.hawaii.edu/optics/kpao/files/KAON/KAON303.pdf
 
@@ -93,20 +129,33 @@ def make_keck_atmospheric_layers(input_grid):
     ----------
     input_grid : Grid
         The input grid of the atmospheric layers.
+    cn_squared : scalar
+        The integrated Cn^2 value for all layers. If this is None (default),
+        then a Fried parameter of 0.2m at 500nm is used.
+    outer_scale : scalar or None
+        The outer scale of the atmosphere. If this is None (default), then an
+        outer scale of 20m is used.
 
     Returns
     -------
     list
         A list of turbulence layers.
     '''
+    if cn_squared is None:
+        cn_squared = Cn_squared_from_fried_parameter(0.2)
+
+    if outer_scale is None:
+        outer_scale = 20
+
     heights = np.array([0, 2100, 4100, 6500, 9000, 12000, 14800])
     velocities = np.array([6.7, 13.9, 20.8, 29.0, 29.0, 29.0, 29.0])
-    outer_scales = np.array([20, 20, 20, 20, 20, 20, 20])
-    Cn_squared = np.array([0.369, 0.219, 0.127, 0.101, 0.046, 0.111, 0.027]) * 1e-12
+    cn_squared_values = np.array([0.369, 0.219, 0.127, 0.101, 0.046, 0.111, 0.027])
+
+    cn_squared_values *= cn_squared / np.sum(cn_squared_values)
 
     layers = []
-    for h, v, cn, L0 in zip(heights, velocities, Cn_squared, outer_scales):
-        layers.append(InfiniteAtmosphericLayer(input_grid, cn, L0, v, h, 2))
+    for h, v, cn in zip(heights, velocities, cn_squared_values):
+        layers.append(InfiniteAtmosphericLayer(input_grid, cn, outer_scale, v, h, 2))
 
     return layers
 

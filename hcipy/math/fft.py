da3b16d4b82907ca84d580e5332b107ca0687c0b
Emiel Por <epor@stsci.edu>
2023-11-16 15:36:22 -0500

    Wrap all FFT functions.

--- hcipy/math/fft.py
+++ hcipy/math/fft.py
@@ -17,46 +17,43 @@ try:
 except ImportError:
     pyfftw = None
 
-@functools.wraps(np.fft.fftn)
-def fftn(*args, threads=None, axes=None, overwrite_x=False, method=None):
-    if method is None:
-        methods = Configuration().fourier.fft.method
-    else:
-        methods = [method]
 
-    if threads is None:
-        threads = _CPU_COUNT
+def _make_func(func_name):
+    if mkl_fft is not None:
+        mkl_func = getattr(mkl_fft, func_name)
 
-    for method in methods:
-        if method == 'mkl' and mkl_fft is not None:
-            return mkl_fft.fftn(*args, axes=axes, overwrite_x=overwrite_x)
-        elif method == 'fftw' and pyfftw is not None:
-            return pyfftw.interfaces.scipy_fft.fftn(*args, axes=axes, workers=threads, overwrite_x=overwrite_x)
-        elif method == 'scipy':
-            return scipy.fft.fftn(*args, axes=axes, workers=threads, overwrite_x=overwrite_x)
-        elif method == 'numpy':
-            return np.fft.fftn(*args, axes=axes)
-    else:
-        raise ValueError('No FFT method could be found.')
+    if pyfftw is not None:
+        pyfftw_func = getattr(pyfftw.interfaces.scipy_fft, func_name)
 
-@functools.wraps(np.fft.ifftn)
-def ifftn(*args, threads=None, axes=None, overwrite_x=False, method=None):
-    if method is None:
-        methods = Configuration().fourier.fft.method
-    else:
-        methods = [method]
+    scipy_func = getattr(scipy.fft, func_name)
+    numpy_func = getattr(np.fft, func_name)
 
-    if threads is None:
-        threads = _CPU_COUNT
+    def func(*args, overwrite_x=False, method=None, threads=None, **kwargs):
+        if method is None:
+            methods = Configuration().fourier.fft.method
+        else:
+            methods = [method]
 
-    for method in methods:
-        if method == 'mkl' and mkl_fft is not None:
-            return mkl_fft.ifftn(*args, axes=axes, overwrite_x=overwrite_x)
-        elif method == 'fftw' and pyfftw is not None:
-            return pyfftw.interfaces.scipy_fft.ifftn(*args, axes=axes, workers=threads, overwrite_x=overwrite_x)
-        elif method == 'scipy':
-            return scipy.fft.ifftn(*args, axes=axes, workers=threads, overwrite_x=overwrite_x)
-        elif method == 'numpy':
-            return np.fft.ifftn(*args, axes=axes)
-    else:
-        raise ValueError('No FFT method could be found.')
+        if threads is None:
+            threads = _CPU_COUNT
+
+        for method in methods:
+            if method == 'mkl' and mkl_fft is not None:
+                return mkl_func(*args, **kwargs, overwrite_x=overwrite_x)
+            elif method == 'fftw' and pyfftw is not None:
+                return pyfftw_func(*args, **kwargs, workers=threads, overwrite_x=overwrite_x)
+            elif method == 'scipy':
+                return scipy_func(*args, **kwargs, workers=threads, overwrite_x=overwrite_x)
+            elif method == 'numpy':
+                return numpy_func(*args, **kwargs)
+        else:
+            raise ValueError('No FFT method could be found.')
+
+    return func
+
+fft = _make_func('fft')
+fft2 = _make_func('fft2')
+fftn = _make_func('fftn')
+ifft = _make_func('ifft')
+ifft2 = _make_func('ifft2')
+ifftn = _make_func('ifftn')

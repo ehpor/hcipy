202063cd3111c8691233be25bb8f9a79f1a67b9b
Emiel Por <epor@stsci.edu>
2023-12-06 03:45:17 -0500

    Select full set of parameters that are supported.

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,10 +2,35 @@ import pytest
 import hcipy
 import numpy as np
 
-@pytest.mark.parametrize('func', ['fftn', 'ifftn', 'rfftn', 'irfftn'])
-@pytest.mark.parametrize('dtype', ['complex128', 'complex64'])
-@pytest.mark.parametrize('method', ['numpy', 'scipy', 'fftw', 'mkl'])
-def test_fft_acceleration(func, method, dtype):
+
+def _parameters():
+    for func in ['fft', 'ifft', 'fft2', 'ifft2', 'fftn', 'ifftn', 'rfft', 'irfft', 'rfft2', 'irfft2', 'rfftn', 'irfftn', 'hfft', 'ihfft']:
+        for dtype_in in ['float32', 'float64', 'complex64', 'complex128']:
+            real_out = func.startswith('ir') or func.startswith('h')
+
+            if dtype_in == 'float32' or dtype_in == 'complex64':
+                dtype_out = 'float32' if real_out else 'complex64'
+            else:
+                dtype_out = 'float64' if real_out else 'complex128'
+
+            if func.startswith('r') and dtype_in.startswith('complex'):
+                continue
+
+            if func.startswith('ih') and dtype_in.startswith('complex'):
+                continue
+
+            for method in ['numpy', 'scipy', 'fftw', 'mkl']:
+                if ('r' in func or 'h' in func) and method == 'mkl':
+                    # MKL doesn't implement rffts or hffts.
+                    continue
+
+                yield (func, method, dtype_in, dtype_out)
+
+@pytest.mark.parametrize('func,method,dtype_in,dtype_out', list(_parameters()))
+def test_fft_acceleration(func, method, dtype_in, dtype_out):
+    rng = np.random.default_rng(seed=0)
+
+    print(func, method, dtype_in, dtype_out)
     if method == 'fftw':
         pytest.importorskip('pyfftw')
 
@@ -14,23 +39,21 @@ def test_fft_acceleration(func, method, dtype):
 
     N = 128
 
-    x = np.random.randn(N, N).astype(dtype)
+    x = rng.standard_normal((N, N)).astype(dtype_in)
 
-    if 'r' not in func and 'complex' in dtype:
-        x = x + 1j * np.random.randn(N, N).astype(dtype)
-
-    if 'r' in func:
-        x = x.real
+    if dtype_in.startswith('complex'):
+        x = x + 1j * rng.standard_normal((N, N)).astype(dtype_in)
 
     numpy_func = getattr(np.fft, func)
     hcipy_func = getattr(hcipy._math.fft, func)
 
-    y_numpy = numpy_func(x)
+    y_numpy = numpy_func(x).astype(dtype_out)
     y_method = hcipy_func(x, method=method)
 
-    if dtype == 'complex64' or dtype == 'float32':
+    if dtype_out == 'complex64' or dtype_out == 'float32':
         rtol = 1e-4
     else:
         rtol = 1e-8
 
-    assert np.allclose(y_numpy, y_method, rtol=rtol)
+    assert np.allclose(y_numpy, y_method, rtol=rtol, atol=rtol)
+    assert y_method.dtype == dtype_out

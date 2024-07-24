01a84ff79b5960fc4bc30b1571bbc1b02ee309d6
yinzi-xin <yxin1025@gmail.com>
2024-07-24 15:32:12 -0700

    Add backwards propagation for fiber nullers.

--- hcipy/optics/fiber_nuller.py
+++ hcipy/optics/fiber_nuller.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ..optics import OpticalElement, PhaseApodizer
+from ..optics import OpticalElement, PhaseApodizer, Wavefront
 from ..propagation import FraunhoferPropagator
 from ..field import make_focal_grid, make_focal_grid, Field, CartesianGrid, RegularCoords
 from ..plotting import imshow_field
@@ -69,9 +69,27 @@ class FiberNuller(OpticalElement):
 
 		return output
 
-	def backwards(self, wavefront):
-		print('This element does not have a backwards propagation.')
-		return NotImplementedError
+	def backward(self, wavefront):
+		'''Propagate backwards through the fiber nuller
+
+		Parameters
+		----------
+		wavefront : scalar
+			The wavefront to propagate. This wavefront is expected to be
+			a complex scalar representing light emerging from the fiber.
+
+		Returns
+		-------
+		Wavefront
+			The pupil plane wavefront.
+		'''
+
+		fib = self.fiber_mode*wavefront
+		foc = Wavefront(fib,wavelength=self.wavelength)
+		collimated = self.prop.backward(foc)
+		output = self.phase_screen_optic.backward(collimated)
+
+		return output
 
 class VortexFiberNuller(FiberNuller):
 	'''A vortex fiber nuller
@@ -160,6 +178,29 @@ class PhotonicLanternNuller(FiberNuller):
 		output = Field(output, self.output_grid)
 		return output
 		
-	def backwards(self, wavefront):
-		print('This element does not have a backwards propagation.')
-		return NotImplementedError
+	def backward(self, wavefront):
+		'''Propagate backwards through the lantern nuller
+
+		Parameters
+		----------
+		wavefront : scalar
+			The wavefront to propagate. This wavefront is expected to be
+			an ndarray of 6 complex scalars representing light emerging
+			from the 6 ports of the lantern.
+
+		Returns
+		-------
+		Wavefront
+			The pupil plane wavefront.
+		'''
+
+		lant = Field(np.zeros(self.lp_modes[0].shape,dtype='complex128'),self.focal_grid)
+		for n in range(len(self.lp_modes)):
+			lant += self.lp_modes[n].astype('complex128')*wavefront[n]
+
+		foc = Wavefront(lant,wavelength=self.wavelength)
+
+		collimated = self.prop.backward(foc)
+		output = self.phase_screen_optic.backward(collimated)
+
+		return output

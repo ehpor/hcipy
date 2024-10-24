fixed sign of reference point for spider

hcipy/aperture/generic.py
@@ -379,8 +379,8 @@ def make_spider_infinite(p, angle, spider_width):
         else:
             x, y = g.coords
 
-        x = x + p[0]
-        y = y + p[1]
+        x = x - p[0]
+        y = y - p[1]
 
         x_new = x * np.cos(spider_angle) + y * np.sin(spider_angle)
         y_new = y * np.cos(spider_angle) - x * np.sin(spider_angle)
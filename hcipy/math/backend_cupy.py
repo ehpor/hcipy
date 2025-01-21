51f1feee012a1bf359e90f3516d8f61c3e081390
Emiel Por <epor@stsci.edu>
2025-01-21 14:54:51 -0800

    Clean up backend files.

--- hcipy/math/backend_cupy.py
+++ hcipy/math/backend_cupy.py
@@ -1,7 +1,6 @@
-from .backend import _functions, _function_wrappers
+from .backend import _functions
 
 def cupy_to_numpy(x):  # pragma: no cover
     return x.get()
 
 _functions['cupy']['to_numpy'] = cupy_to_numpy
-#_function_wrappers['cupy', 'linalg.svd'] = svd_not_full_matrices_wrapper
--- hcipy/math/backend_numpy.py
+++ hcipy/math/backend_numpy.py
@@ -1,10 +1,8 @@
-from .backend import _functions, _function_wrappers, call
+from .backend import _functions, call
 
 def numpy_to_numpy(x):
     return call('asarray', x, like='numpy')
 
 _functions['numpy']['to_numpy'] = numpy_to_numpy
 _functions['builtins']['to_numpy'] = numpy_to_numpy
-#_function_wrappers['numpy', 'linalg.svd'] = svd_not_full_matrices_wrapper
-#_function_wrappers['numpy', 'random.normal'] = with_dtype_wrapper
-#_function_wrappers['numpy', 'random.uniform'] = with_dtype_wrapper
+

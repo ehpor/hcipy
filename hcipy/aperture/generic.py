f8f076285db2f58b07b86e23d4d89e8969ce8120
syhaffert <sebastiaanhaffert@gmail.com>
2025-07-09 14:52:56 +0200

    Add inverted aperture

--- hcipy/aperture/generic.py
+++ hcipy/aperture/generic.py
@@ -480,6 +480,21 @@ def make_shifted_aperture(aperture, shift):
     '''
     return lambda grid: Field(aperture(grid.shifted(-np.array(shift))), grid)
 
+def make_inverted_aperture(aperture):
+    '''Create an inverted version of `aperture`.
+
+    Parameters
+    ----------
+    aperture : Field generator
+        The aperture to invert.
+
+    Returns
+    -------
+    Field generator
+        The inverted aperture.
+    '''
+    return lambda grid: Field(1. - aperture(grid), grid)
+
 def make_segmented_aperture(segment_shape, segment_positions, segment_transmissions=1, return_segments=False):
     '''Create a segmented aperture.
 

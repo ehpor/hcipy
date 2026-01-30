463dea08510ad5ee125a007257960101ed8852fa
Emiel Por <epor@ucsc.edu>
2026-01-30 19:24:58 +0900

    Unwrap keys and values before indexing.

--- hcipy/field/field.py
+++ hcipy/field/field.py
@@ -355,9 +355,13 @@ class NewStyleField(FieldBase):
 
     def __getitem__(self, key, /):
         xp = self.data.__array_namespace__()
+        key = key.data if isinstance(key, NewStyleField) else key
         return NewStyleField(xp.asarray(self.data[key]), self.grid)
 
     def __setitem__(self, key, value, /):
+        key = key.data if isinstance(key, NewStyleField) else key
+        value = value.data if isinstance(value, NewStyleField) else value
+
         self.data[key] = value
 
         return self

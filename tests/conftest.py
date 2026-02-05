d0dbfc583047d2e983899e9a9870b16eeaa13ba7
Emiel Por <epor@ucsc.edu>
2026-02-05 18:19:15 +0900

    Add RandomState class that unifies randomness across backends.

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,33 @@ def disable_user_config():
     hcipy.Configuration().reset(enable_user_overrides=False)
     yield
     hcipy.Configuration().reset()
+
+def get_backend(backend_name):
+    if backend_name == 'numpy':
+        import numpy as np
+        return np
+    elif backend_name == 'cupy':
+        try:
+            import cupy as cp
+            return cp
+        except ImportError:
+            pytest.skip(f'{backend_name} not available')
+    elif backend_name == 'torch':
+        try:
+            import torch
+            return torch
+        except ImportError:
+            pytest.skip(f'{backend_name} not available')
+    elif backend_name == 'jax':
+        try:
+            import jax
+            return jax
+        except ImportError:
+            pytest.skip(f'{backend_name} not available')
+    else:
+        pytest.skip(f'{backend_name} not available')
+
+
+@pytest.fixture(params=['numpy', 'cupy', 'jax', 'torch'])
+def xp(request):
+    return get_backend(request.param)

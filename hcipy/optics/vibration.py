04cd4b6e9a74cbcfd615ee5063ecea7ef782f1fc
Emiel Por <emiel.por@gmail.com>
2026-03-03 10:45:56 +0900

    Add docstrings.

--- hcipy/optics/vibration.py
+++ hcipy/optics/vibration.py
@@ -3,6 +3,25 @@ import numpy as np
 from .optical_element import OpticalElement
 
 class SimpleVibration(OpticalElement):
+    '''A simple vibration model.
+
+    Parameters
+    ----------
+    mode : Field
+        The spatial mode of the vibration.
+    amplitude : scalar
+        The amplitude of the vibration. After multiplying the mode and
+        the amplitude, the resuilting units should be meters.
+    frequency : scalar
+        The temporal frequency of the vibration in Hz.
+    phase_0 : scalar, optional
+        The initial phase of the vibration in radians. Default is 0.
+
+    Attributes
+    ----------
+    t : float
+        The time in seconds.
+    '''
     def __init__(self, mode, amplitude, frequency, phase_0=0):
         self.mode = mode
         self.amplitude = amplitude
@@ -12,6 +31,8 @@ class SimpleVibration(OpticalElement):
 
     @property
     def frequency(self):
+        '''The temporal frequency of the vibration in Hz.
+        '''
         return self._frequency
 
     @frequency.setter
@@ -22,15 +43,43 @@ class SimpleVibration(OpticalElement):
 
     @property
     def phase(self):
+        '''The current phase of the vibration in radians.
+        '''
         return 2 * np.pi * self.frequency * self.t + self.phase_0
 
     def forward(self, wavefront):
+        '''Propagate the wavefront forward through the vibration.
+
+        Parameters
+        ----------
+        wavefront : Wavefront
+            The input wavefront.
+
+        Returns
+        -------
+        Wavefront
+            The output wavefront with the vibration applied.
+
+        '''
         wf = wavefront.copy()
 
         wf.electric_field *= np.exp(1j * (self.mode * self.amplitude / wf.wavelength * np.sin(self.phase)))
         return wf
 
     def backward(self, wavefront):
+        '''Propagate the wavefront backward through the vibration.
+
+        Parameters
+        ----------
+        wavefront : Wavefront
+            The input wavefront.
+
+        Returns
+        -------
+        Wavefront
+            The output wavefront with the inverse vibration applied.
+
+        '''
         wf = wavefront.copy()
 
         wf.electric_field *= np.exp(-1j * (self.mode * self.amplitude / wf.wavelength * np.sin(self.phase)))

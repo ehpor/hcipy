a095348b34cb6dafd66ca3d2a517100f77661fe3
Emiel Por <emiel.por@gmail.com>
2026-06-30 22:32:19 +0200

    Use same function name as the property.

--- hcipy/atmosphere/finite_atmospheric_layer.py
+++ hcipy/atmosphere/finite_atmospheric_layer.py
@@ -150,6 +150,6 @@ class FiniteAtmosphericLayer(AtmosphericLayer):
         return self._L0
 
     @outer_scale.setter
-    def L0(self, L0):  # noqa: N802
+    def outer_scale(self, L0):  # noqa: N802
         self._L0 = L0
         self._noise = None

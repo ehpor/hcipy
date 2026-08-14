75ab0d4435b893e1663812d0cd03d785da7a972f
Emiel Por <emiel.por@gmail.com>
2026-08-14 14:33:29 +0200

    Add phase ramp preparation and application.

---
+++ hcipy/_math/__init__.py
@@ -0,0 +1,11 @@
+from .phase_ramp import (
+    apply_phase_ramp,
+    apply_phase_ramp_numpy,
+    prepare_phase_ramp,
+)
+
+__all__ = [
+    'apply_phase_ramp',
+    'apply_phase_ramp_numpy',
+    'prepare_phase_ramp',
+]
